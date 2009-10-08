63ecfbee551e2270abbdd46ec9e97251ead54bf9
sgarcia <sgarcia@acbc63fc-6c75-0410-9d68-8fa99cba188a>
2009-10-08 14:58:57 +0000

    block adn segment
    git-svn-id: https://neuralensemble.org/svn/neo/trunk@6 acbc63fc-6c75-0410-9d68-8fa99cba188a

--- neo/core/block.py
+++ neo/core/block.py
@@ -1,1 +1,29 @@
 # -*- coding: utf-8 -*-
+
+
+
+class Block(object):
+	"""
+	Top level container for data.
+	
+	**Definition**
+	Main container gathering all the data discrete or continous for a given setup.
+	It can be view as a list of :class:`Segment`.
+	
+	A block is not necessary a homogeneous recorging contrary to :class:`Segment`
+	
+	**Usage**
+	
+	
+	**Example**
+	
+	
+	bl = Block( segments = [seg1 , seg2 , seg3] )
+	
+	bl.get_segments()
+	
+	
+	"""
+	
+	def __init__(self, *arg , **karg):
+		pass
--- neo/core/segment.py
+++ neo/core/segment.py
@@ -1,1 +1,60 @@
 # -*- coding: utf-8 -*-
+
+
+
+class Segment(object):
+	"""
+	
+	
+	**Definition**
+	Container gathering discrete or continous data acquired during the same time lapse.
+	In short, a :class:`Segment` may contain :class:`AnalogSignal`, :class:`SpikeTrain`, :class:`Event` and :class:`Epoch` 
+	that share the same time base.
+	
+	**Usage**
+	
+	
+	**Example**
+	
+	seg = Segment()
+	
+	seg.get_analogsignals()
+	
+	seg.get_spiketrains()
+	
+	seg.get_events()
+	
+	seg.get_epochs()
+	
+	"""
+	
+	def __init__(self, *arg , **karg):
+		pass
+	
+	def get_segments(self):
+		"""
+		Return  :calss:`AnalogSignalList`.
+		"""
+		pass
+		
+	def get_spiketrains(self):
+		"""
+		Return :class:`SpikeTrainList`.
+		"""
+		pass
+		
+	def get_events(self):
+		"""
+		Return a list of :class:`Event`.
+		"""
+		pass
+	
+	def get_epochs(self):
+		"""
+		Return a list of :class:`Epoch`.
+		"""
+		pass	
+
+
+
+
