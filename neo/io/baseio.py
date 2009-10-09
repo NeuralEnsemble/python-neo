9a55927e2073186eda9a9e9ede75c0e5bbd9c2c7
sgarcia <sgarcia@acbc63fc-6c75-0410-9d68-8fa99cba188a>
2009-10-09 10:17:43 +0000

    template class for IO to be discuss
    git-svn-id: https://neuralensemble.org/svn/neo/trunk@20 acbc63fc-6c75-0410-9d68-8fa99cba188a

---
+++ neo/io/baseio.py
@@ -0,0 +1,69 @@
+ # -*- coding: utf-8 -*-
+ 
+ 
+"""
+Class base for IO modules.
+This is a template.
+
+
+ 
+"""
+
+
+
+
+
+class BaseIO(object):
+	"""
+	Classe template to handle Input/Output for different file formats.
+	
+	This template is file reading/writing orented but it can also handle data from/to a database
+	like TDT sytem tanks or SQLite files.
+	
+	Each object is able to declare what can be readed or writen
+	
+	
+	the type can be one of the class defined in neo.core :
+		Block (with all segments, AnalogSignals, SpikeTrains, ...)
+		Segment (with all  AnalogSignals, SpikeTrains, Events, Epoch, ...)
+		SpikeTrain
+		SpikeTrainList
+		AnalogSignal
+		AnalogSignalList
+		Neuron
+	
+	
+	Each IOclass implementation can also add attributs (fields) freely to all object.
+		
+	
+	
+	"""
+	
+	read_possibilty = False
+	write_possibilty = False
+	
+	header_possibility = False
+	
+	type = None
+	
+	read_params = { }
+	write_params = { }
+	
+	def __init__(self , filename = None , **kargs ) :
+		pass
+		
+		
+	
+	def read(self):
+		pass
+		
+		
+	def write(self):
+		pass
+	
+	def read_header():
+		pass
+		
+	def write_header():
+		pass
+	
