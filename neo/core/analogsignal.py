46f1a37635ec2c3c66574b4a29029a29a81f7077
ygordon <ygordon@acbc63fc-6c75-0410-9d68-8fa99cba188a>
2010-11-06 12:42:35 +0000

    Added baseneo and modified analogsignal
    git-svn-id: https://neuralensemble.org/svn/neo/branches/neo0.2@162 acbc63fc-6c75-0410-9d68-8fa99cba188a

--- neo/core/analogsignal.py
+++ neo/core/analogsignal.py
@@ -1,97 +1,65 @@
-# -*- coding: utf-8 -*-
+import numpy as np
+import quantities as pq
+from baseneo import BaseNeo
 
-import numpy
 
-
-
-class AnalogSignal(object):
-     
-    definition = """An :class:`AnalogSignal` is a continuous data signal acquired
-    at time t_start at a certain sampling rate.
+class AnalogSignal(BaseNeo, pq.Quantity):
     """
+    BaseNeo should be the parent of every Neo class
+    Quantities inherits from numpy.ndarray
     
-    __doc__ = """
-    Object to represent an analog signal
-
-    **Definition**"""+definition+"""
-
-    **Usage**
-
-    **Example**
-
+    Usage:
+    a = AnalogSignal([1,2,3], sampling_period=42)
     """
-    
-
-
-    def __init__(self, *arg, **karg):
-        self.signal = numpy.array([], )
-        if karg.has_key('signal'):
-            if type(karg['signal']) == numpy.ndarray or type(karg['signal']) == numpy.memmap :
-                self.signal  = karg['signal']
-        
-
-        for attr in [  'channel' , 'name', 'sampling_rate' , 't_start', 't_stop']:
-            if attr in karg:
-                setattr(self, attr, karg[attr])
+    def __new__(subtype, signal, dtype=None, copy=True, t_start=0., sampling_rate=None, sampling_period=None):
+        # maybe some parameters are useless for the AnalogSignal use case (dtype, copy ?)
+        if not sampling_period is None:
+            if not sampling_rate is None:
+                if sampling_period != 1./sampling_rate:
+                    raise ValueError('The sampling_rate has to be 1./sampling_period')
             else:
-                setattr(self, attr, None)
-        
-        if 'dt' in karg:
-            self.sampling_rate = float(1./karg['dt'])
-        
-        if self.t_start is None:self.t_start = 0.
-        if self.sampling_rate is None:self.sampling_rate = 1.
-            
-        
+                sampling_rate = 1./sampling_period
 
-        if self.t_stop is None and self.sampling_rate !=0.:
-            self.t_stop  = self.t_start + len(self.signal)/self.sampling_rate
+        if isinstance(signal, AnalogSignal):
+            return signal
 
-        self._t = None
+        if isinstance(signal, np.ndarray):
+            new = signal.view(subtype)
+            if copy: return new.copy()
+            else: return new
+
+        if isinstance(signal, str):
+            signal = _convert_from_string(signal)
+
+        # now convert signal to an array
+        arr = pq.Quantity(signal, dtype=dtype, copy=copy)
+
+        # added from quantities before the __new__ because it needs it
+        subtype._dimensionality = arr._dimensionality
+        #subtype.sampling_period = sampling_period no redundancy, thanks
+        subtype.sampling_rate = float(sampling_rate)
+
+        #ret = np.ndarray.__new__(subtype, shape, arr.dtype, buffer=arr, order=order)
+        ret = pq.Quantity.__new__(subtype, arr)
+        ret.signal = ret.view(np.ndarray) #, dtype=arr.dtype)
+        ret.sampling_rate = float(sampling_rate)
+        #ret.sampling_period = sampling_period
+
+        return ret
+
+    @property
+    def sampling_period(self):
+        return 1./self.sampling_rate
 
 
+    @property
+    def t_stop(self):
+        return self.t_start * len(signal[0]) / self.sampling_rate
 
-    #~ def __len__(self):
-        #~ if self.signal is not None :
-            #~ return len(self.signal)
-        #~ else :
-            #~ return 0
-        
-    def compute_time_vector(self) :
-        return numpy.arange(len(self.signal), dtype = 'f8')/self.sampling_rate + self.t_start
+    def __array_finalize__(self, obj):
+        if obj is None: return
+        self.signal = getattr(obj, 'signal', None)
+        self.sampling_period = getattr(obj, 'sampling_period', None)
+        self.sampling_rate = getattr(obj, 'sampling_rate', None)
 
-    def t(self):
-        if self._t==None:
-            self._t=self.compute_time_vector()
-        return self._t
 
-    def max(self):
-        return self.signal.max()
-
-    def min(self):
-        return self.signal.min()
-    
-    def mean(self):
-        return numpy.mean(self.signal)
-    
-    def time_slice(self, t_start, t_stop):
-        """ 
-        Return a new AnalogSignal obtained by slicing between t_start and t_stop
-        
-        Inputs:
-            t_start - begining of the new AnalogSignal, in ms.
-            t_stop  - end of the new AnalogSignal, in ms.
-        
-        See also:
-            interval_slice
-        """
-        assert t_start >= self.t_start
-        assert t_stop <= self.t_stop
-        assert t_stop > t_start
-        
-        t = self.time_axis()
-        i_start = int((t_start-self.t_start)/self.dt)
-        i_stop = int((t_stop-self.t_start)/self.dt)
-        signal = self.signal[i_start:i_stop]
-        result = AnalogSignal(signal, self.dt, t_start, t_stop)
-        return result
