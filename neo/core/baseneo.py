3aafd9e293ef5006bbbe5a92dbce9c122f95ae19
ygordon <ygordon@acbc63fc-6c75-0410-9d68-8fa99cba188a>
2011-02-24 16:06:24 +0000

    Added quantities to every attribute of the QuantifiedAnalogSignal, fixed a bug for the determination of AnalogSignal.t_stop, added a QuantifiedAnalogSignal.__repr__() and modified BaseNeo to not add base attributes to the _anotations (old metadata) dictionary
    git-svn-id: https://neuralensemble.org/svn/neo/branches/neo0.2@186 acbc63fc-6c75-0410-9d68-8fa99cba188a

--- neo/core/baseneo.py
+++ neo/core/baseneo.py
@@ -1,10 +1,26 @@
 class BaseNeo(object):
-    metadata = {}
+    """
+    Bunch: do the same in the other sense
+    Working now:
+      myneoobject.hi = 2
+      >>>myneoobject._anotations['hi'] = 2
+    Have to work in the future:
+      myneoobject._anotations['hi'] = 3
+      >>>myneoobject.hi = 3
+    """
+    def __new__(cls):
+        cls._anotations = {}
+        return cls
 
     def __setattr__(self, k, v):
-        self.metadata[k] = v
+        if not k in self.__dict__.keys():
+            self._anotations[k] = v
         self.__dict__[k] = v
 
     def __delattr__(self, k):
-        del self.metadata[k]
+        if k in self._anotations.keys():
+            del self._anotations[k]
         del self.__dict__[k]
+
+
+
--- neo/core/quantifiedanalogsignal.py
+++ neo/core/quantifiedanalogsignal.py
@@ -1,9 +1,8 @@
 import numpy as np
 import quantities as pq
 from baseneo import BaseNeo
-from analogsignal import AnalogSignal
 
-# maybe it has to be inherited from Neo.AnalogSignal ?
+
 class QuantifiedAnalogSignal(BaseNeo, pq.Quantity):
     """
     This class is usable only if you have installed the Quantities Python package
@@ -11,21 +10,24 @@ class QuantifiedAnalogSignal(BaseNeo, pq.Quantity):
     Quantities inherits from numpy.ndarray
     
     Usages:
-    >>> a = AnalogSignal([1,2,3])
-    >>> b = AnalogSignal([4,5,6], sampling_period=42.)
-    >>> c = AnalogSignal([1,2,3], t_start=42.)
-    >>> d = AnalogSignal([1,2,3], t_start=42., sampling_rate=1/42.])
+    >>> a = QuantifiedAnalogSignal([1,2,3])
+    >>> b = QuantifiedAnalogSignal([4,5,6], sampling_period=42.)
+    >>> c = QuantifiedAnalogSignal([1,2,3], t_start=42.)
+    >>> d = QuantifiedAnalogSignal([1,2,3], t_start=42., sampling_rate=1/42.])
+    >>> e = QuantifiedAnalogSignal([1,2,3], unit='ms')
 
     a.signal : a numpy.ndarray view of the signal
     a.t_start : time when signal begins
-    a.t_stop : t_start + len(signal)
+    a.t_stop : t_start + len(signal) 
     a.sampling_rate : time rate between 2 values
     a.sampling_period : 1./sampling_rate
+    a.units : the units of the attributes
 
     a.metadata : a dictionary of the attributes, updated with __setattr__ and __delattr__ in BaseNeo
     """
-    def __new__(subtype, signal, dtype=None, copy=True, t_start=0., sampling_rate=None, sampling_period=None):
-        # maybe some parameters are useless for the AnalogSignal use case (dtype, copy ?)
+    def __new__(subtype, signal, dtype=None, copy=True, t_start=0., sampling_rate=None, sampling_period=None, unit='ms'):
+        BaseNeo.__new__(subtype)
+        # maybe some parameters are useless for the QuantifiedAnalogSignal use case (dtype, copy ?)
         # add recording point
         if sampling_period is None:
             if sampling_rate is None:
@@ -38,7 +40,10 @@ class QuantifiedAnalogSignal(BaseNeo, pq.Quantity):
                 if sampling_period != 1./sampling_rate:
                     raise ValueError('The sampling_rate has to be 1./sampling_period')
 
-        if isinstance(signal, (AnalogSignal, QuantifiedAnalogSignal)):
+        t_start = t_start * pq.__dict__[unit]
+        sampling_rate = sampling_rate * pq.__dict__[unit]
+
+        if isinstance(signal, QuantifiedAnalogSignal):
             return signal
 
         if isinstance(signal, np.ndarray):
@@ -47,10 +52,11 @@ class QuantifiedAnalogSignal(BaseNeo, pq.Quantity):
             else: return new
 
         if isinstance(signal, str):
+            # be careful about the shape: what is the dimension of my signal ?
             signal = _convert_from_string(signal)
 
         # now convert signal to an array
-        arr = pq.Quantity(signal, dtype=dtype, copy=copy)
+        arr = pq.Quantity(signal, unit, dtype=dtype, copy=copy)
 
         # added _dimensionality from quantities before the __new__ because it needs it
         subtype._dimensionality = arr._dimensionality
@@ -62,19 +68,28 @@ class QuantifiedAnalogSignal(BaseNeo, pq.Quantity):
         ret = pq.Quantity.__new__(subtype, arr)
         ret.signal = ret.view(np.ndarray) #, dtype=arr.dtype)
         ret.t_start = t_start
-        ret.sampling_rate = float(sampling_rate)
+        ret.sampling_rate = sampling_rate
+        ret.unit = ret.dimensionality.string # for BaseNeo._anotations
         #ret.sampling_period = sampling_period
 
         return ret
 
-    @property
+    def __repr__(self):
+        return '<QuantifiedAnalogSignal(\n %s,\n %s, [%s, %s], sampling rate: %s)>' %(self.signal, self.unit, self.t_start, self.t_stop, self.sampling_rate)
+
     def sampling_period(self):
+        # do it in the other sense
         return 1./self.sampling_rate
 
+    def set_sampling_period(self, value):
+        self.sampling_rate = 1./value
+
+    sampling_period = property(fget=sampling_period, fset=set_sampling_period)
 
     @property
     def t_stop(self):
-        return self.t_start * len(self.signal[0]) / self.sampling_rate
+        # do it in both senses
+        return self.t_start + (len(self.signal) * self.sampling_rate)
 
     def __array_finalize__(self, obj):
         if obj is None: return
