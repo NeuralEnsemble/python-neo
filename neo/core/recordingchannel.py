b584044ea4827bd6c963a80dab4dfcf5934e6757
ygordon <ygordon@acbc63fc-6c75-0410-9d68-8fa99cba188a>
2011-02-25 15:44:07 +0000

    changed recordchannel to recordingchannel
    git-svn-id: https://neuralensemble.org/svn/neo/branches/neo0.2@206 acbc63fc-6c75-0410-9d68-8fa99cba188a

--- neo/core/recordingchannel.py
+++ neo/core/recordingchannel.py
@@ -1,6 +1,6 @@
 from neo.core.baseneo import BaseNeo
 
-class RecordChannel(BaseNeo):
+class RecordingChannel(BaseNeo):
     def __init__(self, index, name, analogsignals=[])
         BaseNeo.__init__(self)
         self.name = name
