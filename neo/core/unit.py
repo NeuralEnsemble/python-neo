a22e537b6c35f4355c8ece6c4227291321edc109
ygordon <ygordon@acbc63fc-6c75-0410-9d68-8fa99cba188a>
2011-02-26 11:00:59 +0000

    added recordingchannels to unit
    git-svn-id: https://neuralensemble.org/svn/neo/branches/neo0.2@210 acbc63fc-6c75-0410-9d68-8fa99cba188a

--- neo/core/unit.py
+++ neo/core/unit.py
@@ -1,9 +1,10 @@
 from neo.core.baseneo import BaseNeo
 
 class Unit(BaseNeo):
-    def __init__(self, name='', spike=[], spiketrain=[])
+    def __init__(self, name='', spike=[], spiketrain=[], recordingchannels=[])
         BaseNeo.__init__(self)
         self.name = name
         self.spike = spike
         self.spiketrain = spiketrain
+        self.recordingchannels = recordingchannels
         return self
