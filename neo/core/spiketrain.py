b70931729f3de8e7f0325044096a420fa1a2a483
apdavison <apdavison@acbc63fc-6c75-0410-9d68-8fa99cba188a>
2011-03-04 17:46:26 +0000

    Started implementing `SpikeTrain`
    git-svn-id: https://neuralensemble.org/svn/neo/branches/neo0.2@217 acbc63fc-6c75-0410-9d68-8fa99cba188a

--- neo/core/spiketrain.py
+++ neo/core/spiketrain.py
@@ -1,13 +1,83 @@
 from neo.core.baseneo import BaseNeo
+import quantities as pq
+import numpy
 
 
-class SpikeTrain(BaseNeo):
+def check_has_dimensions_time(*values):
+    errmsgs = []
+    for value in values:
+        if value._reference.dimensionality != pq.s.dimensionality:
+            errmsgs.append("value %s has dimensions %s, not [time]" % (
+                value,value.dimensionality.simplified))
+    if errmsgs:
+        raise ValueError("\n".join(errmsgs))
+
+
+class SpikeTrain(BaseNeo, pq.Quantity):
     """
-    times is a 1D quantified array
-    waveforms is a 3D quantified array with dimensions: element, trodeness, pts
+    An ensemble of action potentials (spikes) emitted by the same unit in a
+    period of time.
+    
+    Always contains the spike times, may also contain the waveforms of the
+    individual action potentials.
+    
+    Inherits from :class:`quantities.Quantity`, which in turn inherits from
+    ``numpy.ndarray``.
     """
-    def __init__(self, times, waveforms=None)
-        BaseNeo.__init__(self)
-        self.times = times
-        self.waveforms = waveforms
-        return self
+    
+    def __new__(cls, times, t_start, t_stop, units='', dtype=None, copy=True,
+                waveforms=None, name='', sort=True):
+        """
+        Create a new :class:`SpikeTrain` instance from a list or numpy array
+        of numerical values, or from a Quantity array with dimensions of time.
+        
+        ``times``      - a list, 1D numpy array or quantity array
+        ``waveforms``  - an :class:`AnalogSignalArray` object (??)
+        ``t_start``    - the start time of the spike train; must be
+                         before or at the same time as the first spike
+        ``t_stop``     - the end time of the spike train; must be after or at
+                         the same time as the last spike
+        ``units``      - if ``times`` is not a quantity array, the units of time
+                         must be specified.
+        """
+        # check units
+        if isinstance(times, pq.Quantity) and units:
+            times = times.rescale(units)
+        if not units and hasattr(times, "units"):
+            units = times.units
+        t_start = pq.Quantity(t_start, units)
+        t_stop = pq.Quantity(t_stop, units)
+        # sort the times and waveforms
+        if sort:
+            sorted = True
+            times = numpy.array(times)
+            sort_indices = numpy.argsort(times)
+            times = times[sort_indices]
+            if waveforms:
+                waveforms = waveforms[sort_indices]
+        else:
+            sorted = False
+        obj = pq.Quantity.__new__(cls, times, units=units, dtype=dtype, copy=copy)
+        check_has_dimensions_time(obj, t_start, t_stop) # should perhaps check earlier
+        obj.t_start = t_start
+        obj.t_stop = t_stop
+        obj._sorted = sorted
+        obj.name = name
+        obj._annotations = {}
+        # check waveforms
+        obj.waveforms = waveforms
+        return obj
+
+    def __array_finalize__(self, obj):
+        super(SpikeTrain, self).__array_finalize__(obj)
+        self.t_start = getattr(obj, 't_start', obj.min())
+        self.t_stop = getattr(obj, 't_stop', obj.max())
+        self.waveforms = getattr(obj, 'waveforms', None)
+
+    @property
+    def duration(self):
+        return self.t_stop - self.t_start
+    
+    @property
+    def sorted(self):
+        return self._sorted
---
+++ neo/test/test_spiketrain.py
@@ -0,0 +1,55 @@
+import unittest
+from neo.core.spiketrain import check_has_dimensions_time, SpikeTrain
+import quantities as pq
+import numpy
+from tools import assert_arrays_equal
+
+class TestFunctions(unittest.TestCase):
+
+    def test__check_has_dimensions_time(self):
+        a = numpy.arange(3) * pq.ms
+        b = numpy.arange(3) * pq.mV
+        c = numpy.arange(3) * pq.mA
+        d = numpy.arange(3) * pq.minute
+        check_has_dimensions_time(a)
+        self.assertRaises(ValueError, check_has_dimensions_time, b)
+        self.assertRaises(ValueError, check_has_dimensions_time, c)
+        check_has_dimensions_time(d)
+        self.assertRaises(ValueError, check_has_dimensions_time, a, b, c, d)
+        
+
+class TestConstructor(unittest.TestCase):
+    
+    def test__create_from_list(self):
+        times = range(10)
+        t_start = 0.0
+        t_stop = 10.0
+        st = SpikeTrain(times, t_start, t_stop, units="ms")
+        self.assertEqual(st.t_start, t_start*pq.ms)
+        self.assertEqual(st.t_stop, t_stop*pq.ms)
+        assert_arrays_equal(st, times*pq.ms)
+
+    def test__create_from_array(self):
+        times = numpy.arange(10)
+        t_start = 0.0*pq.s
+        t_stop = 10000.0*pq.ms
+        st = SpikeTrain(times, t_start, t_stop, units="s")
+        self.assertEqual(st.t_stop, t_stop)
+        assert_arrays_equal(st, times*pq.s)
+
+    def test__create_from_array_without_units_should_raise_ValueError(self):
+        times = numpy.arange(10)
+        t_start = 0.0*pq.s
+        t_stop = 10.0*pq.s
+        self.assertRaises(ValueError, SpikeTrain, times, t_start, t_stop)
+
+    def test__create_from_quantity_array(self):
+        times = numpy.arange(10) * pq.ms
+        t_start = 0.0*pq.s
+        t_stop = 12.0*pq.ms
+        st = SpikeTrain(times, t_start, t_stop)
+        assert_arrays_equal(st, times)
+
+
+if __name__ == "__main__":
+    unittest.main()
