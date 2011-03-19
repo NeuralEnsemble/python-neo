21b3bddfc7a278f21cbef5fe50ae1a4e7115defc
ygordon <ygordon@acbc63fc-6c75-0410-9d68-8fa99cba188a>
2011-03-19 16:15:57 +0000

    fixed some bugs in AnalogSignalArray
    git-svn-id: https://neuralensemble.org/svn/neo/branches/neo0.2@225 acbc63fc-6c75-0410-9d68-8fa99cba188a

--- neo/core/analogsignalarray.py
+++ neo/core/analogsignalarray.py
@@ -49,10 +49,10 @@ class AnalogSignalArray(BaseNeo, pq.Quantity):
 
     def __new__(cls, signal=np.array([ ]) * pq.millivolt, units='', dtype=None, 
                 copy=True, t_start=0*pq.s, sampling_rate=None, sampling_period=None,
-                channel_names= None, channel_indexes = None,
+                channel_names=None, channel_indexes=None,
                 ):
         """
-        Create a new :class:`AnalogSignal` instance from a list or numpy array
+        Create a new :class:`AnalogSignalArray` instance from a list or numpy array
         of numerical values, or from a Quantity array.
         """
         if isinstance(signal, pq.Quantity) and units:
@@ -62,30 +62,30 @@ class AnalogSignalArray(BaseNeo, pq.Quantity):
         obj = pq.Quantity.__new__(cls, signal, units=units, dtype=dtype, copy=copy)
         obj.t_start = t_start
         obj.sampling_rate = _get_sampling_rate(sampling_rate, sampling_period)
-        obj.name = name
+        #obj.name = name
         obj._annotations = {}
         obj._annotations['channel_names'] = channel_names
         obj._annotations['channel_indexes'] = channel_indexes
         return obj
 
     def __array_finalize__(self, obj):
-        super(AnalogSignal, self).__array_finalize__(obj)
+        super(AnalogSignalArray, self).__array_finalize__(obj)
         self.t_start = getattr(obj, 't_start', 0*pq.s)
         self.sampling_rate = getattr(obj, 'sampling_rate', None)
 
     def __repr__(self):
-        return '<AnalogSignal(%s, [%s, %s], sampling rate: %s)>' % (
-             super(AnalogSignal, self).__repr__(), self.t_start, self.t_stop, self.sampling_rate)
+        return '<AnalogSignalArray(%s, [%s, %s], sampling rate: %s)>' % (
+             super(AnalogSignalArray, self).__repr__(), self.t_start, self.t_stop, self.sampling_rate)
 
     def __getslice__(self, i, j):
         # doesn't get called in Python 3 - __getitem__ is called instead
-        obj = super(AnalogSignal, self).__getslice__(i, j)
+        obj = super(AnalogSignalArray, self).__getslice__(i, j)
         obj.t_start = self.t_start + i*self.sampling_period
         return obj
     
     def __getitem__(self, i):
-        obj = super(AnalogSignal, self).__getitem__(i)
-        if isinstance(obj, AnalogSignal):
+        obj = super(AnalogSignalArray, self).__getitem__(i)
+        if isinstance(obj, AnalogSignalArray):
             obj.t_start = self.t_start + i.start*self.sampling_period
         return obj
 
@@ -110,13 +110,13 @@ class AnalogSignalArray(BaseNeo, pq.Quantity):
     def __eq__(self, other):
         if self.t_start != other.t_start or self.sampling_rate != other.sampling_rate:
             return False
-        return super(AnalogSignal, self).__eq__(other)
+        return super(AnalogSignalArray, self).__eq__(other)
 
     def __ne__(self, other):
         return not self.__eq__(other)
 
     def _check_consistency(self, other):
-        if isinstance(other, AnalogSignal):
+        if isinstance(other, AnalogSignalArray):
             for attr in "t_start", "sampling_rate":
                 if getattr(self, attr) != getattr(other, attr):
                     raise Exception("Inconsistent values of %s" % attr)
@@ -128,7 +128,7 @@ class AnalogSignalArray(BaseNeo, pq.Quantity):
 
     def _apply_operator(self, other, op):
         self._check_consistency(other)
-        f = getattr(super(AnalogSignal, self), op)
+        f = getattr(super(AnalogSignalArray, self), op)
         new_signal = f(other)
         new_signal._copy_data_complement(self)
         return new_signal
