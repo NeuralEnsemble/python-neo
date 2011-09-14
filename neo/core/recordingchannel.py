5a122ab4df82487f1e15852ef46de010d7fa38cc
crodger <crodger@acbc63fc-6c75-0410-9d68-8fa99cba188a>
2011-09-14 05:52:16 +0000

    Documentation, requiring required argument index in recording channel
    git-svn-id: https://neuralensemble.org/svn/neo/branches/neo0.2@294 acbc63fc-6c75-0410-9d68-8fa99cba188a

--- neo/core/recordingchannel.py
+++ neo/core/recordingchannel.py
@@ -1,35 +1,47 @@
 from neo.core.baseneo import BaseNeo
 
 class RecordingChannel(BaseNeo):
-    """
-    A RecordingChannel is a container of AnalogSIgnal or SpikeTrain or Unit that come
+    """A RecordingChannel is a container of AnalogSignal that come
     from the same logical and/or physical channel inside a Block.
-    
 
-    Usage:
-    
-    
-    Necessary Attributes/properties:
-        index (int):
-    
-    Recommanded Attributes/properties:
-        coordinate x,y,z (quantitite array):
-        name:
-        description:
-        file_origin:           
-    
-    
     Container of:
         AnalogSignal
-        AnalogSignalArray
-    
-    Container of (many to many relationship):
-        Unit
-        
+        IrregularlySampleSignal
     """
-    def __init__(self, index = 0, **kargs):
+    def __init__(self, index, coordinate=None, **kargs):
+        """Initialize a new RecordingChannel.
+        
+        Usage:
+        # Create a new RecordingChannelGroup and add to current block
+        rcg = RecordingChannelGroup(channel_names=['ch1', 'ch2', 'ch3'])
+        rcg.channel_indexes = [1, 2, 3]
+        block.recordingchannelgroups.append(rcg)
+        
+        rc1 = RecordingChannel(index=1)
+        rcg.recordingchannels.append(rc)
+        rc2 = RecordingChannel(index=2)
+        rcg.recordingchannels.append(rc)
+        rc3 = RecordingChannel(index=3)
+        rcg.recordingchannels.append(rc)        
+        
+        Necessary Attributes/properties:
+            index (int): Index of the channel
+        
+        Recommended Attributes/properties:
+            coordinate (Quantity): x, y, z
+            name: string
+            description: string
+            file_origin: string            
+        """
+        # Inherited initialization
+        # Sets universally recommended attributes, and places all others
+        # in _annotations
         BaseNeo.__init__(self, **kargs)
+        
+        # Store required and recommended attributes
         self.index = index
+        
+        # Initialize contianers
         self.analogsignals = [ ]
         self.irregularysampledsignals = [ ]
 
--- neo/core/recordingchannelgroup.py
+++ neo/core/recordingchannelgroup.py
@@ -2,36 +2,56 @@ from neo.core.baseneo import BaseNeo
 
 class RecordingChannelGroup(BaseNeo):
     """
-    A RecordingChannelGroup is a container inside a Block of some RecordingChannel.
-    It can be usefull for grouping channel that share something in common :
-    an multielectrode array or a tetrode or EEG channel setup.
-    
+    A RecordingChannelGroup is a container inside a Block of some 
+    RecordingChannel objects. It is useful for grouping channels that share
+    something in common: for instance, the channels on a tetrode.
 
     Usage:
+    # Create a new RecordingChannelGroup and add to current block
+    rcg = RecordingChannelGroup(channel_names=['ch1', 'ch2', 'ch3'])
+    rcg.channel_indexes = [1, 2, 3]
+    block.recordingchannelgroups.append(rcg)
     
-    
-    Necessary Attributes/properties:
-    
-    Recommanded Attributes/properties:
-        channel_names:
-        channel_indexes:
-        name:
-        description:
-        file_origin:   
-    
-    Container of:
-        RecordingChannel
-        AnalogSignalArray
-        
-        
-        
+    rc1 = RecordingChannel(index=1)
+    rcg.recordingchannels.append(rc)
+    rc2 = RecordingChannel(index=2)
+    rcg.recordingchannels.append(rc)
+    rc3 = RecordingChannel(index=3)
+    rcg.recordingchannels.append(rc)    
     """
-    def __init__(self, **kargs):
-        BaseNeo.__init__(self, channel_names = None, channel_indexes = None, **kargs)
+    def __init__(self, channel_names=None, channel_indexes=None, **kargs):
+        """Initialize a new RecordingChannelGroup.
         
+        Necessary attributes:
+            None
+    
+        Recommended attributes:
+            channel_names: List of strings naming each channel
+            channel_indexes: List of integer indexes of each channel
+            name: string
+            description: string
+            file_origin: string
+        
+        Container of:
+            RecordingChannel
+            AnalogSignalArray
+        """
+        # Inherited initialization
+        # Sets universally recommended attributes, and places all others
+        # in _annotations
+        BaseNeo.__init__(self, **kargs)
+
+        # Defaults
+        if channel_indexes is None:
+            channel_indexes = []
+        if channel_names is None:
+            channel_names = []
+
+        # Store recommended attributes
         self.channel_names = channel_names
         self.channel_indexes = channel_indexes
         
+        # Initialize containers for child objects
         self.analogsignalarrays = [ ]
         self.recordingchannels = [ ]
 
