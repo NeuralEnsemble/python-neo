bfa4faeb342ee644197569fe7ba75b247d7c8397
crodger <crodger@acbc63fc-6c75-0410-9d68-8fa99cba188a>
2011-09-14 00:43:31 +0000

    Requiring 0m0.000s 0m0.000s 0m0.000s 0m0.000s as argument to SpikeTrain. Implementing required and recommended arguments. Moving as much as possible from __new__ to __init__. All that is required in __new__ is initializing Quantity.
    git-svn-id: https://neuralensemble.org/svn/neo/branches/neo0.2@291 acbc63fc-6c75-0410-9d68-8fa99cba188a

--- neo/core/spiketrain.py
+++ neo/core/spiketrain.py
@@ -25,96 +25,85 @@ class SpikeTrain(BaseNeo, pq.Quantity):
     ``numpy.ndarray``.
     """
     
-    def __new__(cls, times=numpy.array([])*pq.s, t_start=0*pq.s, 
-                t_stop=0*pq.s, units='', dtype=None, copy=True, sort=True,
-                waveforms=None,  left_sweep=None, sampling_rate=None, 
-                name=None, file_origin = None, description = None,
-                ):
-        """
-        Create a new :class:`SpikeTrain` instance from a list or numpy array
-        of numerical values, or from a Quantity array with dimensions of time.
-        
-        Necessary Attributes/properties:
-        
-            ``times``      - a list, 1D numpy array or quantity array
-            
-            ``t_start``    - the start time of the spike train; must be
-                            before or at the same time as the first spike
-            ``t_stop``     - the end time of the spike train; must be after or at
-                            the same time as the last spike
-            ``units``      - if ``times`` is not a quantity array, the units of time
-                            must be specified.
-        
-        Recommended Attributes/properties:
-        ``waveforms``  - a 3D quantities array (spike_index, channel_index, time)
-        sampling_rate = sampling rate of waveforms
-        left_sweep = sometimes the sweep window of each is asymetricly centered (left_sweep need to be define and right_sweep is a propertiy)
-        
-        name:
-        description:
-        file_origin:   
-        
-        
-        """
-        # check units
-        if isinstance(times, pq.Quantity) and units:
-            # It was a quantity array but units were not already specified
+    def __new__(cls, times, units=None, dtype=numpy.float, copy=True, **kwargs):
+        """Constructs Quantity array from data"""
+        # If data is Quantity, rescale to desired units
+        if isinstance(times, pq.Quantity) and units: 
             times = times.rescale(units)
-        if not units and hasattr(times, "units"):
-            # It was a quantity array already containing units info
-            units = times.units
-
-        # Convert start and stop times to Quantity
-        t_start = pq.Quantity(t_start, units)
-        t_stop = pq.Quantity(t_stop, units)
-
-        # sort the times and waveforms
-        if sort:
-            sorted = True
-            times = numpy.array(times)
-            sort_indices = numpy.argsort(times)
-            times = times[sort_indices]
-            if waveforms is not None and waveforms.any():
-                waveforms = waveforms[sort_indices]
-        else:
-            sorted = False
-        obj = pq.Quantity.__new__(cls, times, units=units, dtype=dtype, copy=copy)
-        check_has_dimensions_time(obj, t_start, t_stop) # should perhaps check earlier
-        obj.t_start = t_start
-        obj.t_stop = t_stop
-        obj._sorted = sorted
-        obj.name = name
-        obj.file_origin = file_origin
-        obj.description = description        
-        obj._annotations = {}
-        # check waveforms
-        obj.waveforms = waveforms
-        obj.left_sweep = left_sweep
-        obj.sampling_rate = sampling_rate
+        
+        # Error check that units were provided somehow
+        if units is None: 
+            try:
+                units = times.units
+            except AttributeError:
+                raise ValueError('you must specify units')
+        
+        # Construct Quantity and return
+        obj = pq.Quantity.__new__(cls, times, units=units, dtype=dtype, 
+            copy=copy)
         return obj
 
-    def __init__(self,*args,  **kargs):
-        # this do not call BaseNeo.__init__ because of name= 
-        pass
-
-    def __array_finalize__(self, obj):
-        super(SpikeTrain, self).__array_finalize__(obj)
-        # FIXME bug when empty
-        #~ self.t_start = getattr(obj, 't_start', obj.min())
-        #~ self.t_stop = getattr(obj, 't_stop', obj.max())
-        self.t_start = getattr(obj, 't_start', -numpy.inf*pq.s)
-        self.t_stop = getattr(obj, 't_stop', numpy.inf*pq.s)
-
-
-        self.waveforms = getattr(obj, 'waveforms', None)
-        self.left_sweep = getattr(obj, 'left_sweep', None)
-        self.sampling_rate = getattr(obj, 'sampling_rate', None)
+    
+    def __init__(self, times, t_start=0.0, t_stop=None, 
+        units=None, dtype=numpy.float, copy=True, waveforms=None, 
+        sampling_rate=1.0, left_sweep=None, name='', sort=True, **kwargs):
+        """Create a new SpikeTrain instance from data.
         
+        Required arguments:
+            times : a list, 1d numpy array, or quantity array. 
+            
+            The Quantity array is constructed with the data in `times`, as
+            well as the arguments `units`, `dtype`, and `copy`. 
+            See: SpikeTrain.__new__
+        
+        Recommended arguments:
+            t_start : time at which SpikeTrain began. This will be converted
+                to the same units as the data.
+            t_stop : time at which SpikeTrain ends. If not provided, the
+                maximum of the data is chosen. This will be converted to the
+                same units as the data.
+            waveforms : the waveforms of each spike
+            sampling_rate : the sampling rate of the waveforms
+            left_sweep : hard to explain
+            name : name of the spiketrain
+            sort : if True, the spike train will be sorted
+
+        Any other keyword arguments are stored in the `self.annotations` dict.
+        """
+        # Default value of t_stop
+        if t_stop is None:
+            try: t_stop = self.max()
+            except ValueError: t_stop = 0.0
+
+        # Store recommended attributes
+        self.t_start = pq.Quantity(t_start, units=self.units)
+        self.t_stop = pq.Quantity(t_stop, units=self.units)        
+        self.name = name
+        self.waveforms = numpy.asarray(waveforms, dtype=numpy.float)
+        self.left_sweep = left_sweep
+        self.sampling_rate = float(sampling_rate)
+
+        # Error checking (do earlier?)
+        check_has_dimensions_time(self, self.t_start, self.t_stop)
+
+        # sort the times and waveforms
+        # this should be moved to a SpikeTrain.sort() method
+        if sort:
+            sort_indices = self.argsort()
+            if waveforms is not None and waveforms.any():
+                waveforms = waveforms[sort_indices]
+            self.sort()
+
+        # create annotations dict
+        self._annotations = kwargs
 
     def __repr__(self):
         return '<SpikeTrain(%s, [%s, %s], )>' % (
              super(SpikeTrain, self).__repr__(), self.t_start, self.t_stop, )
 
+    @property
+    def times(self):
+        return self
 
     @property
     def duration(self):
