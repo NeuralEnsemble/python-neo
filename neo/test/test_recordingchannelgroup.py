e15b3936c245cd5a26fd379a32b3fc53de00cddb
crodger <crodger@acbc63fc-6c75-0410-9d68-8fa99cba188a>
2011-09-14 05:54:00 +0000

    adding first tests for recordingchannelgroup
    git-svn-id: https://neuralensemble.org/svn/neo/branches/neo0.2@296 acbc63fc-6c75-0410-9d68-8fa99cba188a

--- neo/test/test_recordingchannelgroup.py
+++ neo/test/test_recordingchannelgroup.py
@@ -10,6 +10,24 @@ except ImportError:
 from neo.core.recordingchannelgroup import RecordingChannelGroup
 
 class TestRecordingChannelGroup(unittest.TestCase):
-    pass 
+    def testInitDefaults(self):
+        rcg = RecordingChannelGroup()
+        self.assertEqual(rcg.name, None)
+        self.assertEqual(rcg.file_origin, None)
+        self.assertEqual(rcg.recordingchannels, [])
+        self.assertEqual(rcg.analogsignalarrays, [])
+        self.assertEqual(rcg.channel_names, [])
+        self.assertEqual(rcg.channel_indexes, [])
+    
+    def testInit(self):
+        rcg = RecordingChannelGroup(file_origin='temp.dat', channel_indexes=[1])
+        self.assertEqual(rcg.file_origin, 'temp.dat')
+        self.assertEqual(rcg.name, None)
+        self.assertEqual(rcg.recordingchannels, [])
+        self.assertEqual(rcg.analogsignalarrays, [])
+        self.assertEqual(rcg.channel_names, [])
+        self.assertEqual(rcg.channel_indexes, [1])        
 
 
+if __name__ == '__main__':
+    unittest.main()
