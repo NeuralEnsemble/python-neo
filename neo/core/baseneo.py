3ec715e54bf1a4f281d7c11d9a70bf9dc69eda7c
crodger <crodger@acbc63fc-6c75-0410-9d68-8fa99cba188a>
2011-09-16 05:32:07 +0000

    Adding documentation, and also removing args from call signature, because documentation says everything should be specified for keyword except for low level data like spike times or analog signal which are handled by child class, so removing args here may catch user bugs
    git-svn-id: https://neuralensemble.org/svn/neo/branches/neo0.2@307 acbc63fc-6c75-0410-9d68-8fa99cba188a

--- neo/core/baseneo.py
+++ neo/core/baseneo.py
@@ -1,31 +1,45 @@
 class BaseNeo(object):
-    """
-    Bunch: do the same in the other sense
-    Working now:
-      myneoobject.hi = 2
-      >>>myneoobject._annotations['hi'] = 2
-    Have to work in the future:
-      myneoobject._annotations['hi'] = 3
-      >>>myneoobject.hi = 3
+    """This is the base class from which all Neo objects inherit.
     
-    # I (SAM) like the idea of creating an instance with any keyword:
-    myneoobject( mywife = 'therese')
+    This class implements support for universally recommended arguments,
+    and also sets up the `_annotations` dict for additional arguments.
     
-      
-      
+    The following "universal" methods are available:
+        __init__ : Grabs the universally recommended arguments `name`,
+            `file_origin`, `description` and stores as attributes.
+            
+            Also stores every additional argument (that is, every argument
+            that is not handled by BaseNeo or the child class), and puts
+            in the dict `_annotations`.
+        
+        __getattr__ : Provides shortcut access by argument name to keys
+            in `_annotations`.
+        
+        annotate(**kwargs) : Updates `_annotations` with keyword/value pairs.
+    
+    Each child class should: 
+        0) call BaseNeo.__init__(self, **kwargs) with all non-recommended
+            (additional) arguments.
+        1) process its necessary arguments in its __new__ or __init__ method
+        2) process its recommended arguments in its __new__ or __init__ method
+    
+    Non-keyword arguments should only be used for necessary arguments.
+    
+    The necessary and recommended arguments for each child class (Neo object)
+    are specified in ../description.py and the documentation for the child.
     """
 
-    def __init__(self, name=None, file_origin = None, description = None,
-                                *args, **kwargs):
-        """This is the base constructor for all neo objects.
+    def __init__(self, name=None, file_origin=None, description=None, **kwargs):
+        """This is the base constructor for all Neo objects.
         
-        For definitions of keyword arguments, see the class documentation
-        for the specific object, ie `neo.AnalogSignal`.
+        Stores universally recommended attributes and creates `_annotations`
+        from additional arguments not processed by BaseNeo or the child class.
         """
+        # create `_annotations` for additional arguments
+        # This funny syntax is to avoid recursion loops with __getattr__
         self.__dict__['_annotations'] = kwargs
-        #self.__dict__['_annotations'] = { }
         
-        # theses attributes are recommended for all objects.
+        # these attributes are recommended for all objects.
         self.name = name
         self.description = description
         self.file_origin = file_origin
