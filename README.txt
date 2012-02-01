7adcaf4e001136635efbaab2e3da32b2609ddf4a
apdavison <apdavison@acbc63fc-6c75-0410-9d68-8fa99cba188a>
2012-02-01 16:22:13 +0000

    Packaging changes - now more specific about what is included in the source package, to avoid including work-in-progress and to reduce the package size (e.g., svg files are excluded, as they are large and not really needed to build the docs)
    git-svn-id: https://neuralensemble.org/svn/neo/trunk@441 acbc63fc-6c75-0410-9d68-8fa99cba188a

--- README.txt
+++ README.txt
@@ -1,26 +1,27 @@
-Read the documentation.
+Neo is a package for representing electrophysiology data in Python, together
+with support for reading a wide range of neurophysiology file formats, including
+Spike2, NeuroExplorer, AlphaOmega, Axon, Blackrock, Plexon, Tdt, and support for
+writing to a subset of these formats plus non-proprietary formats including HDF5.
 
-To build the documentaion type this ina console:
+The goal of neo is to improve interoperability between Python tools for
+analyzing, visualizing and generating electrophysiology data (such as
+OpenElectrophy_, NeuroTools_, G-node_, Helmholtz_, PyNN_) by providing a common,
+shared object model. In order to be as lightweight a dependency as possible,
+neo is deliberately limited to represention of data, with no functions for data
+analysis or visualization.
 
-sudo apt-get python-spinx subversion
-svn https://neuralensemble.org/svn/neo
-cd neo/trunk/doc
-make html
-firefox build/html/index.html
+Neo implements a hierarchical data model well adapted to intracellular and
+extracellular electrophysiology and EEG data with support for multi-electrodes
+(for example tetrodes). Neo's data objects build on the quantities_ package,
+which in turn builds on NumPy by adding support for physical dimensions. Thus
+neo objects behave just like normal NumPy arrays, but with additional metadata,
+checks for dimensional consistency and automatic unit conversion.
 
+Documentation is available at http://packages.python.org/neo/
 
+The project home page is at http://neuralensemble.org/neo
 
-Contributors
-============
+For installation instructions, see doc/source/install.rst
 
-* Thierry Brizzi
-* Andrew Davison
-* Luc Estabanez
-* Samuel Garcia
-* Florent Jaillet
-* Yann Mahnoun
-* Thomas Wachtler
-* Philipp Rautenberg
-* Andrey Sobolev
-* Pierre Yger
-* Yury V. Zaytsev
+:copyright: Copyright 2010-2012 by the Neo team, see AUTHORS.
+:license: Modified BSD License, see LICENSE.txt for details.
--- setup.py
+++ setup.py
@@ -1,62 +1,35 @@
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
-
-
-
-long_description='''
-NEO stands for Neural Ensemble Objects and is a project to provide a common set of base classes to be used
-in neural data analysis, with the aim of getting OpenElectrophy, NeuroTools, GNode, DABBSIP and maybe other projects
-with similar goals more close together.
-
-It provide a set of basic class to manipulate electro-physiological (in vivo and/or simulated) data 
-and an IO module to read/write as many as possible file format.
-
-'''
-
-#~ print find_packages()
-
+from setuptools import setup
 import os
 
 from neo import version as neov
+long_description = open("README.txt").read()
 
-if __name__=="__main__":
-    
-    
-    
-    setup(
-        name = "neo",
-        version = neov.version,
-        packages = find_packages(),
-
-        
-        include_package_data=True,
-        
-        install_requires=[
-                        'numpy>=1.3.0',
-                        #'scipy>=0.7.0',
-                        'quantities>=0.9.0',
-                        ],
-                        
-        requires = [
-                          ],
-        
-        # metadata for upload to PyPI
-        author = "S.garcia, A.Davison, P.Rautenberg, A.Sobolev, F.Jaillet, Y.Mahnoun, T.Brizzi, T.Wachtler, L.Estebannez, P.Yger, Yury V. Zaytsev",
-        author_email = "sgarcia at olfac.univ-lyon1.fr",
-        long_description = long_description,
-        license = "BSD",
-        description = "neo : objects and IO to manipulate electro-physiological (in vivo and/or simulated) data",
-        url='http://neuralensemble.org/trac/neo',
-        classifiers=['Development Status :: 3 - Alpha',
-            'Intended Audience :: Science/Research',
-            'License :: OSI Approved :: BSD License',
-            'Natural Language :: English',
-            'Operating System :: OS Independent',
-            'Programming Language :: Python',
-            'Topic :: Scientific/Engineering :: Bio-Informatics'
-            ]
-    )
+setup(
+    name = "neo",
+    version = neov.version,
+    packages = ['neo', 'neo.core', 'neo.io', 'neo.test', 'neo.test.io'],
+    install_requires=[
+                    'numpy>=1.3.0',
+                    'quantities>=0.9.0',
+                    ],
+    author = "Neo authors and contributors",
+    author_email = "sgarcia at olfac.univ-lyon1.fr",
+    description = "Neo is a package for representing electrophysiology data in Python, together with support for reading a wide range of neurophysiology file formats",
+    long_description = long_description,
+    license = "BSD",
+    url='http://neuralensemble.org/neo',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering']
+)
 
 
 
