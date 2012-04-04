647fc0142a1ea021c31ca71077314f1aca6e9fac
sgarcia <sgarcia@acbc63fc-6c75-0410-9d68-8fa99cba188a>
2012-04-04 08:41:58 +0000

    Patch from aka.tkf@gmail.com
    git-svn-id: https://neuralensemble.org/svn/neo/trunk@461 acbc63fc-6c75-0410-9d68-8fa99cba188a

--- neo/io/spike2io.py
+++ neo/io/spike2io.py
@@ -356,7 +356,7 @@ class Spike2IO(BaseIO):
                 ea.times = alltimes
                 if channelHeader.kind >= 5:
                     # Spike2 marker is closer to label sens of neo
-                    ea.labels = alltrigs['marker'].astype('S')
+                    ea.labels = alltrigs['marker'].astype('S32')
                 if channelHeader.kind == 8:
                     ea.annotate(extra_labels = alltrigs['label'])
                 return ea
