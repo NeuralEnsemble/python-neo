50cb24d2c5bcd56f3005362cd0f43dc1b7208100
Samuel Garcia <sam.garcia.die@gmail.com>
2013-10-08 11:53:54 +0200

    Post 0.3.1 release

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ if os.environ.get('TRAVIS') == 'true' and \
 
 setup(
     name = "neo",
-    version = '0.3.1',
+    version = '0.3.2dev',
     packages = ['neo', 'neo.core', 'neo.io', 'neo.test', 'neo.test.iotest'],
     install_requires=install_requires,
     author = "Neo authors and contributors",
