1a6553619d14f1db53a129a0742cf4d5d86027f3
Todd Jennings <toddrjen@gmail.com>
2013-12-10 16:15:31 +0100

    add force argument to create_many_to_one_relationship to overwrite existing relationships

--- neo/io/tools.py
+++ neo/io/tools.py
@@ -2,13 +2,15 @@
 """
 Tools for IO coder:
   * For creating parent (many_to_one_relationship)
-  * Creating RecordingChannel and making links with AnalogSignals and SPikeTrains
+  * Creating RecordingChannel and making links with AnalogSignals and
+    SPikeTrains
 """
+
 import collections
 import numpy as np
 
-from ..core import *
-from ..description import one_to_many_relationship, many_to_one_relationship
+from neo.core import *
+from neo.description import one_to_many_relationship, many_to_one_relationship
 
 
 #def finalize_block(block):
@@ -22,41 +24,46 @@ from ..description import one_to_many_relationship, many_to_one_relationship
 #            create_many_to_one_relationship(rc)
 
 
-def create_many_to_one_relationship(ob):
+def create_many_to_one_relationship(ob, force=False):
     """
-    When many_to_one relationship when one_to_many exists.
-    Ex: when block.segments it create segment.block
-    It is a utility at the end of creating a Block for IO.
+    Create many_to_one relationship when one_to_many relationships exist.
+    Ex: For each Segment in block.segments it sets segment.block to the
+    parent Block. It is a utility at the end of creating a Block for IO.
 
     Note:
-        this is recursive. It work on Block but also work on others neo objects.
+        This is recursive.
+        It works on Block but also work on others neo objects.
 
     Usage:
     >>> create_many_to_one_relationship(a_block)
+    >>> create_many_to_one_relationship(a_block, force=True)
 
     You want to run populate_RecordingChannel first, because this will create
     new objects that this method will link up.
 
+    If force is True overwrite any existing relationships
+
     """
     # Determine what class was passed, and whether it has children
-    classname =ob.__class__.__name__
-    if classname not in  one_to_many_relationship:
+    classname = ob.__class__.__name__
+    if classname not in one_to_many_relationship:
         # No children
         return
 
     # Iterate through children and build backward links
     for childname in one_to_many_relationship[classname]:
         # Doesn't have links to children
-        if not hasattr(ob, childname.lower()+'s'): continue
+        if not hasattr(ob, childname.lower()+'s'):
+            continue
 
         # get a list of children of type childname and iterate through
         sub = getattr(ob, childname.lower()+'s')
         for child in sub:
             # set a link to parent `ob`, of class `classname`
-            if getattr(child, classname.lower()) is None:
+            if getattr(child, classname.lower()) is None or force:
                 setattr(child, classname.lower(), ob)
             # recursively:
-            create_many_to_one_relationship(child)
+            create_many_to_one_relationship(child, force=force)
 
 
 def populate_RecordingChannel(bl, remove_from_annotation=True):
