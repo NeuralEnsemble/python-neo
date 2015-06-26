c19d376dff9b8959679f309d1d5443fd603b1657
Samuel Garcia <sgarcia@sgarcia-DellXPS13>
2015-06-26 13:12:29 +0200

    Add possible ddl for NeuroshareapiIO

--- neo/io/neuroshareapiio.py
+++ neo/io/neuroshareapiio.py
@@ -16,6 +16,8 @@ from __future__ import absolute_import
 import numpy as np
 import quantities as pq
 
+import os
+
 #check to see if the neuroshare bindings are properly imported    
 try:
     import neuroshare as ns
@@ -85,7 +87,7 @@ class NeuroshareapiIO(BaseIO):
 
 
 
-    def __init__(self , filename = None) :
+    def __init__(self , filename = None, dllpath = None) :
         """
         Arguments:
             filename : the filename
@@ -104,7 +106,12 @@ class NeuroshareapiIO(BaseIO):
         #if a filename was given, create a dictionary with information that will 
         #be needed later on.
         if self.filename != None:
-            self.fd = ns.File(self.filename)
+            if dllpath is not None:
+                name = os.path.splitext(os.path.basename(dllpath))[0]
+                library = ns.Library(name, dllpath)
+            else:
+                library = None
+            self.fd = ns.File(self.filename, library = library)
             #get all the metadata from file
             self.metadata = self.fd.metadata_raw
             #get sampling rate
