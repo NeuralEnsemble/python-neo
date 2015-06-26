9db2d1dbeaca5676c9560c8fed4423aa5deb5e21
Mikkel Elle Lepperød <lep.mik@gmail.com>
2015-06-26 10:43:55 +0200

    rawtrace load finished

--- neo/io/kwikio.py
+++ neo/io/kwikio.py
@@ -2,10 +2,6 @@
 """
 Class for reading data from a .kwik dataset
 
-For the user, it generates a :class:`Segment` or a :class:`Block` with a
-sinusoidal :class:`AnalogSignal`, a :class:`SpikeTrain` and an
-:class:`EventArray`.
-
 Depends on: scipy
 
 Supported: Read
@@ -48,36 +44,10 @@ from neo.core import (Segment, SpikeTrain, Unit, EpochArray,
 # I need to subclass BaseIO
 class KwikIO(BaseIO):
     """
-    Class for "reading" fake data from an imaginary file.
+    Class for "reading" experimental data from a .kwik file.
 
-    For the user, it generates a :class:`Segment` or a :class:`Block` with a
-    sinusoidal :class:`AnalogSignal`, a :class:`SpikeTrain` and an
-    :class:`EventArray`.
-
-    For a developer, it is just an example showing guidelines for someone who wants
-    to develop a new IO module.
-
-    Two rules for developers:
-      * Respect the Neo IO API (:ref:`neo_io_API`)
-      * Follow :ref:`io_guiline`
-
-    Usage:
-        >>> from neo import io
-        >>> r = io.ExampleIO(filename='itisafake.nof')
-        >>> seg = r.read_segment(lazy=False, cascade=True)
-        >>> print(seg.analogsignals)  # doctest: +ELLIPSIS, +NORMALIZE_WHITESPACE
-        [<AnalogSignal(array([ 0.19151945,  0.62399373,  0.44149764, ...,  0.96678374,
-        ...
-        >>> print(seg.spiketrains)    # doctest: +ELLIPSIS, +NORMALIZE_WHITESPACE
-         [<SpikeTrain(array([ -0.83799524,   6.24017951,   7.76366686,   4.45573701,
-            12.60644415,  10.68328994,   8.07765735,   4.89967804,
-        ...
-        >>> print(seg.eventarrays)    # doctest: +ELLIPSIS, +NORMALIZE_WHITESPACE
-        [<EventArray: TriggerB@9.6976 s, TriggerA@10.2612 s, TriggerB@2.2777 s, TriggerA@6.8607 s, ...
-        >>> anasig = r.read_analogsignal(lazy=True, cascade=False)
-        >>> print(anasig._data_description)
-        {'shape': (150000,)}
-        >>> anasig = r.read_analogsignal(lazy=False, cascade=False)
+    Generates a :class:`Segment` with a :class:`AnalogSignal`,
+    a :class:`SpikeTrain` and a :class:`EpochArray`.
 
     """
 
@@ -103,7 +73,7 @@ class KwikIO(BaseIO):
     has_header         = False
     is_streameable     = False
 
-    name               = 'kwik'
+    name               = 'Kwik'
 
     extensions         = [ 'kwd', 'kwx', 'kwik' ]
 
@@ -119,138 +89,109 @@ class KwikIO(BaseIO):
 
         """
         BaseIO.__init__(self)
-        self.filename = filename
+        self._filename = filename
         basename, ext = op.splitext(filename)
-        self.kwik = h5py.File(filename, 'r')
-        self.kwd = h5py.File(basename + '.raw.kwd', 'r')
+        self._kwik = h5py.File(filename, 'r')
+        self._kwd = h5py.File(basename + '.raw.kwd', 'r')
 
     def read_segment(self,
                      lazy=False,
                      cascade=True,
                      dataset=0,
-                     lfpchannel=12,
-                     rawchannel=None
+                     channel_index=None,
+                     sampling_rate=None
                     ):
         """
-        lfpchannel can be int  - how to select all?
+        Channel_index can be int, iterable or None to select all
         """
 
-        raw_shape = self.kwd['recordings'][str(dataset)]['data'].shape # Do we need this?
-        attrs = self.kwd['recordings'][str(dataset)].attrs
+        raw_shape = self._kwd['recordings'][str(dataset)]['data'].shape # Do we need this?
+        attrs = self._kwd['recordings'][str(dataset)].attrs
         # create an empty segment
         seg = Segment( name='session something' )
 
         if cascade:
-            if LFPchannel is not None:
             # read nested analosignal
-                ana = self.read_lfpdata(lazy=lazy,
-                                        kwd=kwd,
-                                        channel_index=lfpchannel,
-                                        dataset=dataset,
-                                             )
-                seg.analogsignals += [ ana ]
-            if rawchannel is not None:
-                # read nested analosignal
-                ana = self.read_rawdata(lazy=lazy,
-                                        kwd=kwd,
-                                        channel_index=rawchannel,
-                                        dataset=dataset,
-                                             )
-                seg.analogsignals += [ ana ]
+            ana = self._read_traces(lazy=lazy,
+                                    cascade=cascade,
+                                    dataset=dataset,
+                                    channel_index=channel_index,
+                                    sampling_rate=sampling_rate,
+                                         )
+            seg.analogsignals += [ ana ]
             # read nested spiketrain
             num_spiketrain_by_channel = 3
             for i in range(raw_shape[1]):
                 for _ in range(num_spiketrain_by_channel):
-                    sptr = self.read_spiketrain(lazy=lazy,
+                    sptr = self._read_spiketrain(lazy=lazy,
                                                 channel_index=i,
                                                 )
                     seg.spiketrains += [ sptr ]
 
-            epo = self.read_stimulus(lazy=lazy)
+            epo = self._read_stimulus(lazy=lazy)
             seg.epocharrays += [ epo ]
 
-            seg.duration = (raw_shape[0]
+            seg.duration = (raw_shape[0] #TODO: this duration is not necessarily correct after downsample
                           / attrs['sample_rate']
-                          + attrs['start_time'])
+                          + attrs['start_time']) * pq.s
 
         seg.create_many_to_one_relationship()
         return seg
 
-    def read_lfpdata(self,
-                          lazy=False,
-                          cascade=True,
-                          channel_index=12,
-                          dataset=0,
-                          sampling_rate=1000
-                          bandwidth=[0,500]
-                          ):
+    def _read_traces(self,
+                      lazy=False,
+                      cascade=True,
+                      dataset=0,
+                      channel_index=None,
+                      sampling_rate=None
+                      ):
         """
-        Downsample and filter raw data into LFP
+        read raw traces with given sampling_rate, if sampling_rate is None
+        default from acquisition system is given. channel_index can be int or
+        iterable, if None all channels are read
         """
+        raw_shape = self._kwd['recordings'][str(dataset)]['data'].shape
+        attrs = self._kwd['recordings'][str(dataset)].attrs
+
+        if sampling_rate:
+            sliceskip = int(attrs['sample_rate']/sampling_rate)
+        else:
+            sliceskip = 1
+            sampling_rate = attrs['sample_rate']
+
+        if channel_index is not None:
+            if type(channel_index) is int: channel_index = [channel_index]
+        else:
+            channel_index = range(0,raw_shape[1])
 
         if lazy:
             anasig = AnalogSignal([],
                                   units='uV',
                                   sampling_rate=sampling_rate*pq.Hz,
                                   t_start=attrs['start_time']*pq.s,
-                                  channel_index=channel_index,
+                                  channel_index=np.array(channel_index),
                                   )
             # we add the attribute lazy_shape with the size if loaded
-            anasig.lazy_shape = self.kwd['recordings'][str(dataset)]['data'].shape[0]
+            anasig.lazy_shape = raw_shape #TODO: wrong if downsampled
         else:
+            data_array = []
             for idx in channel_index:
-                attrs = self.kwd['recordings'][str(dataset)].attrs
-                slice = int(attrs['sample_rate']/sampling_rate)
-                data = np.array(self.kwd['recordings'][str(dataset)]['data'][0:-1:slice,idx])
-                data = self.lowpass_filter(bits2volt(data), bandwidth)
-                anasig = AnalogSignalArray(data,
-                                      units='uV',
-                                      sampling_rate=sampling_rate*pq.Hz,
-                                      t_start=attrs['start_time']*pq.s,
-                                      channel_index=idx,
-                                      )
-
-        # for attributes out of neo you can annotate
-        anasig.annotate(info = 'low pass filtered')
-
-        return anasig
-
-    def read_rawdata(self,
-                          lazy=False,
-                          cascade=True,
-                          channel_index=0,
-                          dataset=0,
-                          ):
-        """
-
-        """
-
-        attrs = self.kwd['recordings'][str(dataset)].attrs
-
-        if lazy:
-            anasig = AnalogSignal([],
-                                  units='uV',
-                                  sampling_rate=attrs['sample_rate']*pq.Hz,
-                                  t_start=attrs['start_time']*pq.s,
-                                  channel_index=channel_index,
-                                  )
-            # we add the attribute lazy_shape with the size if loaded
-            anasig.lazy_shape = self.kwd['recordings'][str(dataset)]['data'].shape[0]
-        else:
-            data   = bits2volt(np.array(self.kwd['recordings'][str(dataset)]['data'][:,:]))
+                data = self._kwd['recordings'][str(dataset)]['data'].value[:,idx]
+                data_array.append(data[0:-1:sliceskip])
+            data = self._bits2volt(np.array(data_array).T)
+            data_array = [] #delete from memory
             anasig = AnalogSignalArray(data,
-                                  units='uV',
-                                  sampling_rate=attrs['sample_rate']*pq.Hz,
-                                  t_start=attrs['start_time']*pq.s,
-                                  channel_index=channel_index,
-                                  )
+                                       units='uV',
+                                       sampling_rate=sampling_rate*pq.Hz,
+                                       t_start=attrs['start_time']*pq.s,
+                                       channel_index=np.array(channel_index),
+                                       )
 
         # for attributes out of neo you can annotate
-        anasig.annotate(info = 'rawdata')
-
+        anasig.annotate(info='raw traces')
         return anasig
 
-    def read_stimulus(self, lazy):
+    def _read_stimulus(self, lazy):
         epo = EpochArray()
         if lazy:
             # in lazy case no data are read
@@ -269,15 +210,15 @@ class KwikIO(BaseIO):
 
         return epo
 
-    def bits2volt(bits):
+    def _bits2volt(self,bits):
         '''
         Converts 16bit signal to microvolt
         '''
         # TODO: verify gain, formula and bitvalue
         gain = 200
-        return (bits * 20e6) / (gain * np.pow(2.0,16));
+        return bits * 20e6 / (gain * 2.0**16);
 
-    def read_spiketrain(self,
+    def _read_spiketrain(self,
                         lazy = False,
                         cascade = True,
                         segment_duration = 15.,
@@ -285,7 +226,7 @@ class KwikIO(BaseIO):
                         channel_index = 0,
                         ):
         """
-        With this IO SpikeTrain can e acces directly with its channel number
+        With this IO SpikeTrain can be accessed directly with its channel number
         """
         # There are 2 possibles behaviour for a SpikeTrain
         # holding many Spike instance or directly holding spike times
