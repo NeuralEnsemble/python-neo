31e03db76513f873c3ad68a066bbe613a03c51c1
Mikkel Elle Lepperoed <lep.mik@gmail.com>
2015-07-08 11:06:03 +0200

    made test for KwikIO

---
+++ neo/test/iotest/test_kwikio.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Tests of neo.io.kwikio
+"""
+
+# needed for python 3 compatibility
+from __future__ import division
+
+import sys
+
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+from neo.io import KwikIO
+from neo.test.iotest.common_io_test import BaseTestIO
+
+
+@unittest.skipIf(sys.version_info[0] > 2, "not Python 3 compatible")
+class TestKwikIO(BaseTestIO, unittest.TestCase):
+    ioclass = KwikIO
+    files_to_test = ['experiment0.kwik',
+                    #  'experiment0.raw.kwd',
+                    #  'experiment0.kwx',
+                     'test_hybrid_120sec.kwik',
+                    #  'test_hybrid_120sec.raw.kwd',
+                    #  'test_hybrid_120sec.kwx'
+                     ]
+    files_to_download = files_to_test
+
+
+if __name__ == "__main__":
+    unittest.main()
