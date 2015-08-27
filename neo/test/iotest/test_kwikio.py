a6e58c757aaa918eaa40c5f059a24a20b312a99c
Mikkel Elle Lepperød <lep.mik@gmail.com>
2015-08-27 15:26:15 +0200

    added necessary files to download in test_kwikio

--- neo/test/iotest/test_kwikio.py
+++ neo/test/iotest/test_kwikio.py
@@ -18,9 +18,10 @@ from neo.test.iotest.common_io_test import BaseTestIO
 
 class TestKwikIO(BaseTestIO, unittest.TestCase):
     ioclass = KwikIO
-    files_to_test = ['experiment1.kwik',
-                     ]
-    files_to_download = files_to_test
+    files_to_test = ['experiment1.kwik']
+    files_to_download =  ['experiment1.kwik',
+                          'experiment1.kwx',
+                          'experiment1_100.raw.kwd']
 
 
 if __name__ == "__main__":
