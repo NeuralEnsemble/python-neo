d1970789c85fea5f9e30c7b063b668fe3e190d26
Andrew Davison <andrew.davison@unic.cnrs-gif.fr>
2016-07-05 10:45:08 +0200

    Change version number for the `apibreak` branch to 0.5.0dev

--- neo/version.py
+++ neo/version.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-version = '0.4.0dev'
+version = '0.5.0dev'
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ if os.environ.get('TRAVIS') == 'true' and \
 
 setup(
     name = "neo",
-    version = '0.4.0dev',
+    version = '0.5.0dev',
     packages = ['neo', 'neo.core', 'neo.io', 'neo.test', 'neo.test.iotest'],
     install_requires=install_requires,
     author = "Neo authors and contributors",
