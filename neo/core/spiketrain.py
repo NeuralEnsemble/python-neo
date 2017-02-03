99ae9ee82f46fbcc652248fe32cba4d005501669
grg2rsr <grg2rsr@gmail.com>
2017-02-03 10:51:08 +0100

    reimplementing __add__ method for spiketrains for shifting all spike trains by a constant amount of time

--- neo/core/spiketrain.py
+++ neo/core/spiketrain.py
@@ -427,6 +427,23 @@ class SpikeTrain(BaseNeo, pq.Quantity):
             obj.waveforms = obj.waveforms[i:j]
         return obj
 
+    def __add__(self,time):
+        '''
+        Shifts the time point of all spikes by the amount in :attr:`time`.
+
+        Raises an exception of new time points fall outside :attr:`t_start` or
+        :attr:`t_stop`
+        '''
+        spikes = self.view(pq.Quantity)
+        check_has_dimensions_time(time)
+        _check_time_in_range(spikes+time, self.t_start, self.t_stop)
+        return SpikeTrain(times=spikes+time, t_stop=self.t_stop, units=self.units,
+                          sampling_rate=self.sampling_rate,
+                          t_start=self.t_start, waveforms=self.waveforms,
+                          left_sweep=self.left_sweep, name=self.name,
+                          file_origin=self.file_origin,
+                          description=self.description, **self.annotations)
+
     def __getitem__(self, i):
         '''
         Get the item or slice :attr:`i`.
