4811b4eb3ac9141360794e5f1079640e44b71900
Samuel Garcia <sam.garcia.die@gmail.com>
2018-01-10 15:27:35 +0100

    do not add corrdinate if radius is 0

--- neo/rawio/brainvisionrawio.py
+++ neo/rawio/brainvisionrawio.py
@@ -121,9 +121,8 @@ class BrainVisionRawIO(BaseRawIO):
         for c in range(sig_channels.size):
             coords = vhdr_header['Coordinates']['Ch{}'.format(c+1)]
             coords = [float(v) for v in coords.split(',')]
-            
-            self.raw_annotations['signal_channels'][c]['coordinates'] = coords
-            self.raw_annotations['signal_channels'][c]['coordinates_mode'] = 'polar'
+            if coords[0]!=0.:
+                self.raw_annotations['signal_channels'][c]['coordinates'] = coords
     
     def _source_name(self):
         return self.filename
