3ee9c9b27f688341e025d2d0193231ed7d5d01d7
Björn Müller <bj.mueller@fz-juelich.de>
2018-05-23 16:36:01 +0200

    Added file for tests for DataObject class and especially array annotations

---
+++ neo/test/coretest/test_dataobject.py
@@ -0,0 +1,26 @@
+
+import unittest
+
+class Test_DataObject(unittest.TestCase):
+    def test(self):
+        pass
+
+
+class Test_array_annotations(unittest.TestCase):
+    def test_check_arr_ann(self):
+        # Ensure size, ensure array, single dimension,
+        # Wrong size, nested array, scalar, correct array, also multiple anns,
+        pass
+
+    def test_array_annotate(self):
+        # Make sure it functions correctly, not much to check, most is done in check_arr_ann
+        pass
+
+    def test_arr_anns_at_index(self):
+        # Get them, test for desired type and size, content
+        pass
+
+    def test_getitem(self):
+        # Get them, test for desired type and size, content
+        pass
+
