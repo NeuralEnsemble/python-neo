85665647abded7d5fee505d9a0465fa3cb1ac36f
Chek Yin Choi <chekchek2007@gmail.com>
2018-08-03 12:24:42 +0200

    Add the new NixIO which uses the BaseFromRawIO

---
+++ neo/io/nixio_fr.py
@@ -0,0 +1,16 @@
+from neo.io.basefromrawio import BaseFromRaw
+from neo.rawio.nixrawio import NIXRawIO
+
+#This class subjects to limitations when there are multiple asymmetric blocks
+
+
+class NixIO(NIXRawIO, BaseFromRaw):
+
+    name = 'NIX IO'
+
+    _prefered_signal_group_mode = 'group-by-same-units'
+    _prefered_units_group_mode = 'all-in-one'
+
+    def __init__(self, filename):
+        NIXRawIO.__init__(self, filename)
+        BaseFromRaw.__init__(self, filename)
