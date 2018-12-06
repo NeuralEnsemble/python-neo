2b680638636def3dddf859633bacd6b4e9152b46
Chek Yin Choi <chekchek2007@gmail.com>
2018-12-06 17:14:59 +0100

    get filename path

--- neo/test/iotest/test_nixio_fr.py
+++ neo/test/iotest/test_nixio_fr.py
@@ -28,7 +28,7 @@ class TestNixfr(BaseTestIO, unittest.TestCase, ):
 
     def setUp(self):
         super(TestNixfr, self).setUp()
-        self.testfilename = 'nixio_fr.nix'
+        self.testfilename = self.get_filename_path('nixio_fr.nix')
         self.reader_fr = NixIOfr(filename=self.testfilename)
         self.reader_norm = NixIO(filename=self.testfilename, mode='ro')
         self.blk = self.reader_fr.read_block(block_index=1, load_waveforms=True)
