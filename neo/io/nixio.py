fe34316e6b09fbda0f64a209abd2048d18cd79ce
Julia Sprenger <julia.sprenger@rwth-aachen.de>
2019-01-14 16:20:41 +0100

    Update import of ProxyObjects

--- neo/io/nixio.py
+++ neo/io/nixio.py
@@ -34,7 +34,7 @@ import numpy as np
 from .baseio import BaseIO
 from ..core import (Block, Segment, ChannelIndex, AnalogSignal,
                     IrregularlySampledSignal, Epoch, Event, SpikeTrain, Unit)
-from ..io.basefromrawio.proxyobjects import BaseProxy
+from ..io.proxyobjects import BaseProxy
 from ..version import version as neover
 
 try:
