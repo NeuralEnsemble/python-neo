dc3518e6883c5e8f7fcd3a841133d33e23e35bd8
Elodie Legouée <elodie.legouee@unic.cnrs-gif.fr>
2019-03-13 16:11:07 +0100

    NWB Files names

--- neo/rawio/tests/test_nwbrawio.py
+++ neo/rawio/tests/test_nwbrawio.py
@@ -17,10 +17,10 @@ class TestNWBRawIO(BaseTestRawIO, unittest.TestCase, ):
     rawioclass = NWBRawIO
     files_to_download = [
 
-              '/home/elodie/envNWB/NWB_files/my_example_2.nwb', # Very simple file nwb create by me only TimeSeries
-#              '/home/elodie/envNWB/NWB_files/brain_observatory.nwb', # nwb file given by Matteo Cantarelli
-#              '/home/elodie/envNWB/NWB_files/mynwb.h5', # nwb file given by Lungsi
-#              '/home/elodie/envNWB/NWB_files/GreBlu9508M_Site1_Call1.nwb',
+              '/home/elodie/NWB_Files/my_example_2.nwb', # Very simple file nwb create by me only TimeSeries
+#              '/home/elodie/NWB_Files/brain_observatory.nwb', # nwb file given by Matteo Cantarelli
+#              '/home/elodie/NWB_Files/mynwb.h5', # nwb file given by Lungsi
+#              '/home/elodie/NWB_Files/GreBlu9508M_Site1_Call1.nwb',
 
 		     ] 
     entities_to_test = files_to_download
