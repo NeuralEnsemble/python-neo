73931e8228427ded8a2b64fda1067a8726d0e970
Elodie Legouée <elodie.legouee@unic.cnrs-gif.fr>
2019-07-18 15:00:22 +0200

    Read NWB files and tests

--- neo/io/nwbio.py
+++ neo/io/nwbio.py
@@ -74,8 +74,8 @@ class NWBIO(BaseIO):
         """
         print("*** __init__ ***")
         BaseIO.__init__(self, filename=filename)
-#        BaseIO.__init__(self)
-        print("filename = ", filename)
+        #BaseIO.__init__(self)
+        #print("filename = ", filename)
         self.filename = filename
         io = pynwb.NWBHDF5IO(self.filename, mode='r') # Open a file with NWBHDF5IO
         self._file = io.read() # Define the file as a NWBFile object
@@ -83,15 +83,15 @@ class NWBIO(BaseIO):
     def read_block(self, lazy=False, cascade=True, **kwargs):
         print("*** read_block ***")
         self._lazy = lazy
-        print("lazy = ", lazy)
+        #print("lazy = ", lazy)
         file_access_dates = self._file.file_create_date
-        print("file_access_dates = ", file_access_dates)
+        #print("file_access_dates = ", file_access_dates)
         identifier = self._file.identifier # or experimenter ?
-        print("identifier = ", identifier)
+        #print("identifier = ", identifier)
         if identifier == '_neo':  # this is an automatically generated name used if block.name is None
             identifier = None
         description = self._file.session_description # or experiment_description ?
-        print("description = ", description)
+        #print("description = ", description)
         if description == "no description":
             description = None
         block = Block(name=identifier, 
@@ -105,21 +105,21 @@ class NWBIO(BaseIO):
         print("block = ", block)
         if cascade:
             self._handle_general_group(block)
-            self._handle_epochs_group(block)
+            self._handle_epochs_group(lazy, block) #self._handle_epochs_group(block)
             self._handle_acquisition_group(lazy, block) # self._handle_acquisition_group(block)
             self._handle_stimulus_group(lazy, block) # self._handle_stimulus_group(block)
             self._handle_processing_group(block)
             self._handle_analysis_group(block)
         self._lazy = False
         return block
-        print("Return block = ", block)
 
     def _handle_general_group(self, block):
         print("*** def _handle_general_group ***")
         #block.annotations['file_read_log'] += ("general group not handled\n")
 
-    def _handle_epochs_group(self, block):
+    def _handle_epochs_group(self, lazy, block):
         print("*** def _handle_epochs_group ***")
+        self._lazy = lazy
         # Note that an NWB Epoch corresponds to a Neo Segment, not to a Neo Epoch.
 ###        epochs = self._file.epochs
         epochs = self._file.acquisition
@@ -129,50 +129,65 @@ class NWBIO(BaseIO):
         ##for name, epoch in epochs.items():
 #        for name, epoch in epochs:
         for key in epochs:    
-            print("key = ", key)
+            #print("key = ", key)
             #print("epochs = ", epochs)
              # todo: handle epoch.attrs.get('links')
             timeseries = []
-            print("timeseries = ", timeseries)
+            #print("timeseries = ", timeseries)
             current_shape = self._file.get_acquisition(key).data.shape[0] # sample number
             #print("current_shape = ", current_shape)
             times = np.zeros(current_shape)
-           #for key, value in epoch.items(): 
+           #for key, value in epoch.items():
             for j in range(0, current_shape):
                 times[j]=1./self._file.get_acquisition(key).rate*j+self._file.get_acquisition(key).starting_time
                 if times[j] == self._file.get_acquisition(key).starting_time:
                     t_start = times[j] * pq.second
-                    print("t_start = ", t_start)
+                    #print("t_start = ", t_start)
                 elif times[j]==times[-1]:
                     t_stop = times[j] * pq.second
-                    print("t_stop = ", t_stop)
+                    #print("t_stop = ", t_stop)
                 else:
                     # todo: handle value['count']
                     # todo: handle value['idx_start']
                     #timeseries.append(self._handle_timeseries(key, value.get('timeseries')))
                     timeseries.append(self._handle_timeseries(self._lazy, key, times[j]))
+                    #print(timeseries)
+                    #print("timeSeries 1 bis = ", timeseries)
+                #print(timeseries)
+                #print("timeseries 2nd bis = ", timeseries)
 #            segment = Segment(name=name)
                 segment = Segment(name=j)
-                print("segment = ", segment)
-################################################################################
-            #for obj in timeseries:
-            for obj in self._file.get_acquisition:
+                #print("segment = ", segment)
+            for obj in timeseries:
+                #print("obj = ", obj)
+                #print("timeseries = ", timeseries)
+#            for obj in self._file.acquisition:
+###                print("obj in segment = ", obj)
                 obj.segment = segment
-                print("obj.segment = ", obj.segment)
+#                segment==obj.segment
+                #print("segment in loop = ", segment)
+#                print("obj.segment = ", obj.segment)
                 if isinstance(obj, AnalogSignal):
+                    print("*** isinstance(obj, AnalogSignal) ***")
                     segment.analogsignals.append(obj)
+                    #print("obj=AnalogSignal")
                 elif isinstance(obj, IrregularlySampledSignal):
+                    print("*** isinstance(obj, IrregularlySampledSignal) ***")
                     segment.irregularlysampledsignals.append(obj)
+                    #print("obj=IrregularlySampledSignal")
                 elif isinstance(obj, Event):
+                    print("*** isinstance(obj, Event) ***")
                     segment.events.append(obj)
+                    #print("obj=Event")
                 elif isinstance(obj, Epoch):
+                    print("*** isinstance(obj, Epoch) ***")
                     segment.epochs.append(obj)
+                    #print("obj=Epoch")
             segment.block = block
-            print("segment.block = ", segment.block)
-            block.segments.append(segment)
-            print("segment = ", segment)
-            print("block.segments.append(segment) = ", block.segments.append(segment))
-################################################################################
+            #print("segment = ", segment)
+            #print("block = ", block)
+#            block.segments.append(segment)
+            return obj, segment
 
 
     def _handle_timeseries(self, lazy, name, timeseries):
@@ -180,7 +195,6 @@ class NWBIO(BaseIO):
         # todo: check timeseries.attrs.get('schema_id')
         # todo: handle timeseries.attrs.get('source')
 #         subtype = timeseries.attrs['ancestry'][-1]
-
         for i in self._file.acquisition:
             data_group = self._file.get_acquisition(i).data
             #print("data_group = ", data_group)
@@ -189,9 +203,9 @@ class NWBIO(BaseIO):
             #if self._lazy:
             if lazy==True:
                 data = np.array((), dtype=dtype)
-                print("data if lazy = ", data)
+                #print("data if lazy = ", data)
                 lazy_shape = data_group.shape  # inefficient to load the data to get the shape
-                print("lazy_shape = ", lazy_shape)
+                #print("lazy_shape = ", lazy_shape)
             else:
                 data = data_group
             if dtype.type is np.string_:
@@ -199,9 +213,9 @@ class NWBIO(BaseIO):
                     times = np.array(())
                 else:
                     times = self._file.get_acquisition(i).timestamps
-                    print("times in timestamps = ", times)
+                    #print("times in timestamps = ", times)
                 duration = 1/self._file.get_acquisition(i).rate
-                print("************************ duration = ", duration)
+                #print("************************ duration = ", duration)
                 if durations:
                     # Epoch
                     if self._lazy:
@@ -210,18 +224,18 @@ class NWBIO(BaseIO):
                                 durations=durations,
                                 labels=data,
                                 units='second')
-                    print("obj if duration = ", obj)
+                    #print("obj if duration = ", obj)
                 else:
                     # Event
                     obj = Event(times=times,
                                 labels=data,
                                 units='second')
-                    print("obj Event = ", obj)
+                    #print("obj Event = ", obj)
             else:
                 #units = get_units(data_group)
                 units = self._file.get_acquisition(i).unit
                 #print("units = ", units)
-            current_shape = self._file.get_acquisition(i).data.shape[0] # sample number
+            current_shape = self._file.get_acquisition(i).data.shape[0] # number of samples
             #print("current_shape = ", current_shape)
             times = np.zeros(current_shape)
             for j in range(0, current_shape):
@@ -229,11 +243,11 @@ class NWBIO(BaseIO):
                 if times[j] == self._file.get_acquisition(i).starting_time:
                     # AnalogSignal
                     sampling_metadata = times[j]
-                    print("sampling_metadata = ", sampling_metadata)        
+                    #print("sampling_metadata = ", sampling_metadata)        
                     t_start = sampling_metadata * pq.s
-                    print("t_start = ", t_start)
+                    #print("t_start = ", t_start)
                     sampling_rate = self._file.get_acquisition(i).rate * pq.Hz
-                    print("sampling_rate = ", sampling_rate)
+                    #print("sampling_rate = ", sampling_rate)
                     #assert sampling_metadata.attrs.get('unit') == 'Seconds'
 ###                    assert sampling_metadata.unit == 'Seconds'                    
 #                 # todo: handle data.attrs['resolution']
@@ -242,7 +256,7 @@ class NWBIO(BaseIO):
                                        sampling_rate=sampling_rate,
                                        t_start=t_start,
                                        name=name)
-                    print("obj = ", obj)
+                    #print("obj = ", obj)
 #                elif 'timestamps' in timeseries:
                 elif self._file.get_acquisition(i).timestamps:
                     # IrregularlySampledSignal
@@ -259,7 +273,7 @@ class NWBIO(BaseIO):
 #                 raise Exception("Timeseries group does not contain sufficient time information")
 #         if self._lazy:
 #             obj.lazy_shape = lazy_shape
-#         return obj
+            return obj
 
 
     def _handle_acquisition_group(self, lazy, block):
@@ -277,44 +291,44 @@ class NWBIO(BaseIO):
         ###segment_acq = dict((segment.name, segment) for segment in block.segments)
         ###print("segment_acq = ", segment_acq)
         for name in acq:
-            print("name = ", name) # Sample number 'index_'
+            #print("name = ", name) # Sample number 'index_'
 #             if name == 'unit_list':
 #                 pass  # todo
 #             else:
 #            segment_name = name
             # Note that an NWB Epoch corresponds to a Neo Segment, not to a Neo Epoch.
             segment_name = self._file.epochs
-            print("segment_name = ", segment_name)
+            #print("segment_name = ", segment_name)
             desc = self._file.get_acquisition(name).unit
-            print("desc = ", desc)
+            #print("desc = ", desc)
 ###            segment = segment_acq[segment_name]
             segment = segment_name
 #            print("segment = ", segment)
             #if self._lazy:
             if lazy==True:
                 times = np.array(())
-                print("times = ", times)
+                #print("times = ", times)
                 #lazy_shape = group['times'].shape
                 lazy_shape = self._file.get_acquisition(name).data.shape
-                print("lazy_shape = ", lazy_shape)
+                #print("lazy_shape = ", lazy_shape)
             else:
                 current_shape = self._file.get_acquisition(name).data.shape[0] # sample number
-                print("current_shape = ", current_shape)
+                #print("current_shape = ", current_shape)
                 times = np.zeros(current_shape)
                 for j in range(0, current_shape): # For testing !
                     times[j]=1./self._file.get_acquisition(name).rate*j+self._file.get_acquisition(name).starting_time # temps = 1./frequency [Hz] + t_start [s]
-                print("times[j] = ", times)
+                #print("times[j] = ", times)
                 spiketrain = SpikeTrain(times, units=pq.second,
                                          t_stop=times[-1]*pq.second)  # todo: this is a custom Neo value, general NWB files will not have this - use segment.t_stop instead in that case?
-                print("spiketrain = ", spiketrain)
+                #print("spiketrain in _handle_acquisition_group = ", spiketrain)
             #if self._lazy:
 ###            if lazy==True:
 ###                spiketrain.lazy_shape = lazy_shape
             if segment is not None:
                 spiketrain.segment = segment
-                print("segment = ", segment)
+                #print("segment = ", segment)
                 segment.spiketrains.append(spiketrain)
-
+        return spiketrain
 
     def _handle_stimulus_group(self, lazy, block):
         print("*** def _handle_stimulus_group ***")
@@ -330,42 +344,42 @@ class NWBIO(BaseIO):
 ###        segment_sti = dict((segment.name, segment) for segment in block.segments)
 ###        print("segment_sti = ", segment_sti)
         for name in sti:
-            print("name = ", name) # Sample number 'index_'
+            #print("name = ", name) # Sample number 'index_'
 #             if name == 'unit_list':
 #                 pass  # todo
 #             else:
 #            segment_name = name
             # Note that an NWB Epoch corresponds to a Neo Segment, not to a Neo Epoch.
             segment_name_sti = self._file.epochs
-            print("segment_name_sti = ", segment_name_sti)
+            #print("segment_name_sti = ", segment_name_sti)
             desc_sti = self._file.get_stimulus(name).unit
-            print("desc_sti = ", desc_sti)
+            #print("desc_sti = ", desc_sti)
 ###            segment = segment_acq[segment_name]
             segment_sti = segment_name_sti
-            print("segment_sti = ", segment_sti)
+            #print("segment_sti = ", segment_sti)
             #if self._lazy:
             if lazy==True:
                 times = np.array(())
-                print("times = ", times)
+                #print("times = ", times)
                 #lazy_shape = group['times'].shape
                 lazy_shape = self._file.get_stimulus(name).data.shape
-                print("lazy_shape = ", lazy_shape)
+                #print("lazy_shape = ", lazy_shape)
             else:
                 current_shape = self._file.get_stimulus(name).data.shape[0] # sample number
-                print("current_shape = ", current_shape)
+                #print("current_shape = ", current_shape)
                 times = np.zeros(current_shape)
                 for j in range(0, current_shape): # For testing !
                     times[j]=1./self._file.get_stimulus(name).rate*j+self._file.get_stimulus(name).starting_time # temps = 1./frequency [Hz] + t_start [s]
-                print("times[j] = ", times)
+                #print("times[j] = ", times)
                 spiketrain = SpikeTrain(times, units=pq.second,
                                          t_stop=times[-1]*pq.second)  # todo: this is a custom Neo value, general NWB files will not have this - use segment.t_stop instead in that case?
-                print("spiketrain = ", spiketrain)
+                #print("spiketrain = ", spiketrain)
             #if self._lazy:
 ###            if lazy==True:
 ###                spiketrain.lazy_shape = lazy_shape
             if segment_sti is not None:
                 spiketrain.segment_sti = segment_sti
-                print("segment_sti = ", segment_sti)
+                #print("segment_sti = ", segment_sti)
                 segment_sti.spiketrains.append(spiketrain)
 
 
@@ -442,11 +456,11 @@ prefix_map = {
 
 def get_units(data_group):
     print("*** def get_units ***")
-    print("data_group = ", data_group)
+    #print("data_group = ", data_group)
 #     conversion = data_group.attrs.get('conversion')
     #base_units = data_group.attrs.get('unit')
     base_units = data_group.units
-    print("base_units = ", base_units)
+    #print("base_units = ", base_units)
 #     return prefix_map[conversion] + base_units
 
 
