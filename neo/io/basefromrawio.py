6e9013cb622c2e2b3690010daa0bbae9806b531e
Samuel Garcia <sam.garcia.die@gmail.com>
2019-07-24 15:59:00 +0200

    more clear

--- neo/io/basefromrawio.py
+++ neo/io/basefromrawio.py
@@ -108,8 +108,8 @@ class BaseFromRaw(BaseIO):
         if signal_group_mode is None:
             signal_group_mode = self._prefered_signal_group_mode
             if self._prefered_signal_group_mode == 'split-all':
-                self.logger.warning("the default signal_group_mode will'\
-                        'be 'group-by-same-units' in next release")
+                self.logger.warning("the default signal_group_mode will change from "\
+                                "'split-all' to 'group-by-same-units' in next release")
 
         if units_group_mode is None:
             units_group_mode = self._prefered_units_group_mode
