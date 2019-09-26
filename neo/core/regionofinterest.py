b43026dbd603307a2add4c634c7ecd6071ddef95
Andrew Davison <andrew.davison@unic.cnrs-gif.fr>
2019-09-26 10:55:59 +0200

    Make RegionOfInterest work with float arguments

--- neo/core/regionofinterest.py
+++ neo/core/regionofinterest.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
-class RegionOfInterest:
+
+from math import floor, ceil
+
+
+class RegionOfInterest(object):
+    """Abstract base class"""
     pass
 
 
@@ -12,9 +17,9 @@ class CircularRegionOfInterest(RegionOfInterest):
     >>> signal = image_sequence.signal_from_region(roi)
 
     *Required attributes/properties*:
-        :x, y: (integers)
+        :x, y: (integers or floats)
             Pixel coordinates of the centre of the ROI
-        :radius: (integer)
+        :radius: (integer or float)
             Radius of the ROI in pixels
     """
 
@@ -24,21 +29,27 @@ class CircularRegionOfInterest(RegionOfInterest):
         self.x = x
         self.radius = radius
 
-    def is_inside(self, circle_x, circle_y, rad, x, y):
+    @property
+    def centre(self):
+        return (self.x, self.y)
 
-        if ((x - circle_x) * (x - circle_x) +
-                (y - circle_y) * (y - circle_y) <= rad * rad):
+    @property
+    def center(self):
+        return self.centre
+
+    def is_inside(self, x, y):
+        if ((x - self.x) * (x - self.x) +
+                (y - self.y) * (y - self.y) <= self.radius * self.radius):
             return True
         else:
             return False
 
     def pixels_in_region(self):
-
+        """Returns a list of pixels whose *centres* are within the circle"""
         pixel_in_list = []
-        for y in range(self.y - self.radius, self.y + self.radius + 1):
-            for x in range(self.x - self.radius, self.x + self.radius + 1):
-
-                if self.is_inside(self.x, self.y, self.radius, x, y):
+        for y in range(int(floor(self.y - self.radius)), int(ceil(self.y + self.radius))):
+            for x in range(int(floor(self.x - self.radius)), int(ceil(self.x + self.radius))):
+                if self.is_inside(x, y):
                     pixel_in_list.append([x, y])
 
         return pixel_in_list
@@ -55,9 +66,9 @@ class RectangularRegionOfInterest(RegionOfInterest):
     *Required attributes/properties*:
         :x, y: (integers or floats)
             Pixel coordinates of the centre of the ROI
-        :width: (integer)
+        :width: (integer or float)
             Width (x-direction) of the ROI in pixels
-        :height: (integer)
+        :height: (integer or float)
             Height (y-direction) of the ROI in pixels
     """
 
@@ -67,15 +78,22 @@ class RectangularRegionOfInterest(RegionOfInterest):
         self.width = width
         self.height = height
 
-    def pixels_in_region(self):
+    def is_inside(self, x, y):
+        if (self.x - self.width/2.0 <= x < self.x + self.width/2.0
+            and self.y - self.height/2.0 <= y < self.y + self.height/2.0):
+            return True
+        else:
+            return False
 
+    def pixels_in_region(self):
+        """Returns a list of pixels whose *centres* are within the rectangle"""
         pixel_list = []
         h = self.height
         w = self.width
-        for y in range(self.y - (int(h / 2)), self.y + (int(h / 2))):
-            for x in range(self.x - (int(w / 2)), self.x + (int(w / 2))):
-                pixel_list.append([x, y])
-
+        for y in range(int(floor(self.y - h / 2.0)), int(ceil(self.y + h / 2.0))):
+            for x in range(int(floor(self.x - w / 2.0)), int(ceil(self.x + w / 2.0))):
+                if self.is_inside(x, y):
+                    pixel_list.append([x, y])
         return pixel_list
 
 
@@ -92,7 +110,9 @@ class PolygonRegionOfInterest(RegionOfInterest):
     >>> signal = image_sequence.signal_from_region(roi)
 
     *Required attributes/properties*:
-        :vertices: tuples containing the (x, y) coordinates of the vertices of the polygon
+        :vertices:
+            tuples containing the (x, y) coordinates, as integers or floats,
+            of the vertices of the polygon
     """
 
     def __init__(self, *vertices):
@@ -129,7 +149,8 @@ class PolygonRegionOfInterest(RegionOfInterest):
 
     def pixels_in_region(self):
 
-        min_x, max_x, min_y, max_y = self.vertices[0][0], self.vertices[0][0], self.vertices[0][1], self.vertices[0][1]
+        min_x, max_x, min_y, max_y = (self.vertices[0][0], self.vertices[0][0],
+                                      self.vertices[0][1], self.vertices[0][1])
 
         for i in self.vertices:
             if i[0] < min_x:
@@ -140,9 +161,10 @@ class PolygonRegionOfInterest(RegionOfInterest):
                 min_y = i[1]
             if i[1] > max_y:
                 max_y = i[1]
+
         list_coord = []
-        for y in range(min_y, max_y):
-            for x in range(min_x, max_y):
+        for y in range(int(floor(min_y)), int(ceil(max_y))):
+            for x in range(int(floor(min_x)), int(ceil(max_x))):
                 list_coord.append((x, y))
 
         pixel_list = self.polygon_ray_casting(self.vertices, list_coord)
