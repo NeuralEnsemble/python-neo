54cc13b8c1fb395d8e03d294d5b61efd25660b6e
Andrew Davison <andrew.davison@unic.cnrs-gif.fr>
2019-10-03 10:18:22 +0200

    Update copyright statements
    [skip ci]

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -52,7 +52,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Neo'
-copyright = u'2010-2018, ' + AUTHORS
+copyright = u'2010-2019, ' + AUTHORS
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
