67505347cdb7dc86100255fbf98c5bb29b78c662
Andrew Davison <andrew.davison@unic.cnrs-gif.fr>
2020-03-04 17:13:33 +0100

    fix 'rescale' method for Event and Epoch

--- neo/core/epoch.py
+++ neo/core/epoch.py
@@ -187,9 +187,19 @@ class Epoch(DataObject):
         Return a copy of the :class:`Epoch` converted to the specified
         units
         '''
+        # Use simpler functionality, if nothing will be changed
+        dim = pq.quantity.validate_dimensionality(units)
+        if self.dimensionality == dim:
+            return self.copy()
 
-        obj = super(Epoch, self).rescale(units)
-        obj._durations = obj.durations.rescale(units)
+        # Rescale the object into a new object
+        obj = self.duplicate_with_new_data(times=self.view(pq.Quantity).rescale(dim),
+                                           durations=self.durations.rescale(dim),
+                                           labels=self.labels,
+                                           units=units)
+
+        # Expected behavior is deepcopy, so deepcopying array_annotations
+        obj.array_annotations = deepcopy(self.array_annotations)
         obj.segment = self.segment  # not sure we should do this
         return obj
 
