d6ea59fcd56df090fea028cfa077f8722273a763
Samuel Garcia <sam.garcia.die@gmail.com>
2020-11-05 22:15:38 +0100

    add some annoation and channel location handling.

--- neo/rawio/spikeglxrawio.py
+++ neo/rawio/spikeglxrawio.py
@@ -13,10 +13,18 @@ Contrary to other implementations this read the entire folder and subfolder so:
     This the "device"
   * For imec device both "ap" and "lf" are extracted so one device have several "stream"
 
+Note:
+  * there are sevral version depending the NP probe generatio 1.0 2.0 3.0 
+     I am not sure that the meta file is almost the same for all.
+     Maybe there some differences, this have to be check.
+     This IO have been tested basically wit neuropixel (NP) 2.0 one shank
+
 
 # TODO:
   * contact SpkeGLX developer to see how to deal with absolut t_start when several segment
   * contact SpkeGLX developer to understand the last channel SY0 function
+  * better handling of annotations at object level by sub group of device (need rawio change)
+  * better handling of channel location
 
 
 See:
@@ -58,19 +66,15 @@ class SpikeGLXRawIO(BaseRawIO):
     def _parse_header(self):
         
         self.signals_info_list = scan_files(self.dirname)
-        print(len(self.signals_info_list))
-        print([info['stream_name'] for info in self.signals_info_list])
         
-        stream_names = list(np.unique([info['stream_name'] for info in self.signals_info_list]))
-        # TODO order stream_names depending the sampling_rate higher sampling_rate should be first
+        # sort stream_name by hiher sampling rate first
+        srates = {info['stream_name']: info['sampling_rate'] for info in self.signals_info_list}
+        stream_names = sorted(list(srates.keys()), key=lambda e: srates[e])[::-1]
+        
         nb_segment = np.unique([info['seg_index'] for info in self.signals_info_list]).size
         
         self._memmaps = {}
         self.signals_info_dict = {}
-        self._global_channel_to_stream = {}
-        self._global_channel_to_local_channel = []
-        sig_channels = []
-        global_chan = 0
         for info in self.signals_info_list:
             # key is (seg_index, stream_name)
             key = (info['seg_index'], info['stream_name'])
@@ -80,8 +84,20 @@ class SpikeGLXRawIO(BaseRawIO):
             data = np.memmap(info['bin_file'], dtype='int16', mode='r',
                         shape=(info['sample_length'], info['num_chan']), offset=0, order='C')
             self._memmaps[key] = data
+
+        # create channel header
+        self._global_channel_to_stream = {}
+        self._global_channel_to_local_channel = []
+        self._channel_location = {}
+        sig_channels = []
+        global_chan = 0
+        signal_annotations = []
+        for stream_name in stream_names:
+            # take first segment
+            info = self.signals_info_dict[0, stream_name]
             
             group_id = stream_names.index(info['stream_name'])
+            print('stream_name', stream_name, group_id)
             
             # add channels to global list
             for local_chan in range(info['num_chan']):
@@ -90,7 +106,16 @@ class SpikeGLXRawIO(BaseRawIO):
                 chan_name = info['channel_names'][local_chan]
                 sig_channels.append((chan_name, global_chan, info['sampling_rate'], 'int16', info['units'], 
                                     info['channel_gains'][local_chan], info['channel_offsets'][local_chan], group_id))
-                                    
+                
+                # annotation
+                ann = {}
+                ann['stream'] = info['stream_name']
+                signal_annotations.append(ann)
+                
+                # channel location
+                if 'channel_location' in info:
+                    self._channel_location[info['seg_index'], info['device']] = info['channel_location']
+                
                 # the channel id is a global counter and so equivalent to channel_index
                 # this is bad : this should be changed by an id base on a str
                 global_chan += 1
@@ -125,10 +150,18 @@ class SpikeGLXRawIO(BaseRawIO):
 
         # insert some annotation at some place
         self._generate_minimal_annotations()
-        
-        # TODO in annotations :
-        #  *  channel location
-        #  * name, stream_name, stream_kind, datatime
+        self._generate_minimal_annotations()
+        block_ann = self.raw_annotations['blocks'][0]
+        block_ann['file_origin'] = self.dirname
+
+        for seg_index in range(nb_segment):
+            seg_ann = block_ann['segments'][seg_index]
+            seg_ann['file_origin'] = self.dirname
+            seg_ann['name'] = "Segment {}".format(seg_index)
+
+            for c in range(sig_channels.size):
+                anasig_an = seg_ann['signals'][c]
+                anasig_an.update(signal_annotations[c])
 
     def _segment_t_start(self, block_index, seg_index):
         return 0.
@@ -160,6 +193,14 @@ class SpikeGLXRawIO(BaseRawIO):
         
         return raw_signals
 
+    def get_channel_location(self, seg_index=0, device=None):
+        if device is None:
+            if len(self._channel_location) ==1:
+                return list(self._channel_location.values())[0]
+            else:
+                raise ValueError('device must specified')
+        return self._channel_location[seg_index, device]
+
 
 def scan_files(dirname):
     """
@@ -188,12 +229,11 @@ def scan_files(dirname):
             r = re.findall('_g(\d*)_t', name)
             seg_index =  int(r[0][0])
             device = file.split('.')[1]
-            print(meta_filename)
             if 'imec' in device:
                 signal_kind = file.split('.')[2]
                 stream_name = device + '.' + signal_kind
                 units = 'uV'
-                # please note the 1e-6 in gain for this uV
+                # please note the 1e6 in gain for this uV
 
                 # metad['imroTbl'] contain two gain per channel  AP and LF
                 # except for the last fake channel
@@ -206,8 +246,8 @@ def scan_files(dirname):
                     # the last channel don't have gain
                     per_channel_gain[c] = 1. / float(meta['imroTbl'][c].split(' ')[index_imroTbl])
                 gain_factor = float(meta['imAiRangeMax']) / 512
-                channel_gains = per_channel_gain * gain_factor * 1e-6
-
+                channel_gains = per_channel_gain * gain_factor * 1e6
+                
             else:
                 signal_kind = ''
                 stream_name = device
@@ -239,9 +279,18 @@ def scan_files(dirname):
             info['stream_name'] = stream_name
             info['units'] = units
             info['channel_names'] = [txt.split(';')[0] for txt in meta['snsChanMap']]
-            #TODO
             info['channel_gains'] = channel_gains
             info['channel_offsets'] = np.zeros(info['num_chan'])
+            
+            if signal_kind == 'ap':
+                channel_location = []
+                for e in meta['snsShankMap']:
+                        x_pos = int(e.split(':')[1])
+                        y_pos = int(e.split(':')[2])
+                        #~ channel_location.append([x_pos*x_pitch, y_pos*y_pitch])
+                        channel_location.append([x_pos, y_pos])
+                
+                info['channel_location'] = np.array(channel_location)
 
             l.append(info)
 
