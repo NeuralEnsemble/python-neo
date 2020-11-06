922e89378d0eea236266f90bbd8862d754494580
Peter N. Steinmetz <PeterNSteinmetz@steinmetz.org>
2020-11-06 09:29:10 -0700

    Read and test with partial blocks. Test all types in test_neuralyxio.

--- neo/test/iotest/test_neuralynxio.py
+++ neo/test/iotest/test_neuralynxio.py
@@ -24,12 +24,12 @@ class CommonNeuralynxIOTest(BaseTestIO, unittest.TestCase, ):
     files_to_test = [
         'BML/original_data',
         'BML_unfilledsplit/original_data',
+        'Cheetah_v1.1.0/original_data',
         'Cheetah_v4.0.2/original_data',
         'Cheetah_v5.5.1/original_data',
         'Cheetah_v5.6.3/original_data',
         'Cheetah_v5.7.4/original_data',
-        'Pegasus_v2.1.1',
-        'Cheetah_v6.3.2/incomplete_blocks']
+        'Pegasus_v2.1.1']
     files_to_download = [
         'BML/original_data/CSC1_trunc.Ncs',
         'BML/plain_data/CSC1_trunc.txt',
@@ -37,6 +37,9 @@ class CommonNeuralynxIOTest(BaseTestIO, unittest.TestCase, ):
         'BML_unfilledsplit/original_data/unfilledSplitRecords.Ncs',
         'BML_unfilledsplit/plain_data/unfilledSplitRecords.txt',
         'BML_unfilledsplit/README.txt',
+        'Cheetah_v1.1.0/original_data/CSC67_trunc.Ncs',
+        'Cheetah_v1.1.0/README.txt',
+        'Cheetah_v1.1.0/plain_data/CSC67_trunc.txt',
         'Cheetah_v4.0.2/original_data/CSC14_trunc.Ncs',
         'Cheetah_v4.0.2/plain_data/CSC14_trunc.txt',
         'Cheetah_v4.0.2/README.txt',
@@ -259,31 +262,32 @@ class TestPegasus_v211(CommonNeuralynxIOTest, unittest.TestCase):
 
 class TestData(CommonNeuralynxIOTest, unittest.TestCase):
 
-    def load_plaindata(self, filename, numSamps):
-        '''
+    def _load_plaindata(self, filename, numSamps):
+        """
         Load numSamps samples only from Ncs dump files which contain one row for each record,
         each row containing the timestamp, channel number, whole integer sampling frequency,
         number of samples, followed by that number of samples (which may be different for
         each record).
-        '''
+        """
         res = []
         with open(filename) as f:
             for line in f:
                 vals = list(map(int, line.split()))
                 numSampsThisLine = len(vals) - 4
-                if numSampsThisLine < 0 or numSampsThisLine != vals[3]:
+                if numSampsThisLine < 0 or numSampsThisLine < vals[3]:
                     raise IOError('plain data file "' + filename + ' improperly formatted')
-                if numSampsThisLine < numSamps - len(res):
-                    res.append(vals[4:])
+                numAvail = min(numSampsThisLine, vals[3])  # only use valid samples
+                if numAvail < numSamps - len(res):
+                    res.append(vals[4:(4+numAvail)])
                 else:
-                    res.append(vals[4:(3+numSamps-len(res))])
+                    res.append(vals[4:(4+numSamps-len(res))])
                 if len(res) == numSamps:
                     break
 
             return [item for sublist in res for item in sublist]
 
     def test_ncs(self):
-        for session in self.files_to_test[1:2]:  # in the long run this should include all files
+        for session in self.files_to_test:
             dirname = self.get_filename_path(session)
             nio = NeuralynxIO(dirname=dirname, use_cache=False)
             block = nio.read_block()
@@ -298,12 +302,12 @@ class TestData(CommonNeuralynxIOTest, unittest.TestCase):
                 filename = nio.ncs_filenames[chuid][:-3] + 'txt'
                 filename = filename.replace('original_data', 'plain_data')
                 overlap = 512 * 500
-                plain_data = self.load_plaindata(filename, overlap)
+                plain_data = self._load_plaindata(filename, overlap)
                 gain_factor_0 = plain_data[0] / anasig.magnitude[0, 0]
                 numToTest = min(len(plain_data), len(anasig.magnitude[:, 0]))
                 np.testing.assert_allclose(plain_data[:numToTest],
                                            anasig.magnitude[:numToTest, 0] * gain_factor_0,
-                                           rtol=0.01)
+                                           rtol=0.01, err_msg=" for file " + filename)
 
     def test_keep_original_spike_times(self):
         for session in self.files_to_test:
