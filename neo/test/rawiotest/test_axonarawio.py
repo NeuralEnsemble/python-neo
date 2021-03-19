ef4e283be39116351e2ad2a800ffcdbe2b5e4235
sbuergers <sbuergers@gmail.com>
2021-03-18 20:25:28 -0400

    changed files_to_download to only include base filename

--- neo/test/rawiotest/test_axonarawio.py
+++ neo/test/rawiotest/test_axonarawio.py
@@ -14,10 +14,7 @@ from neo.test.rawiotest.common_rawio_test import BaseTestRawIO
 
 class TestAxonaRawIO(BaseTestRawIO, unittest.TestCase, ):
     rawioclass = AxonaRawIO
-    files_to_download = [
-        'axona_raw.set', 
-        'axona_raw.bin'
-    ]
+    files_to_download = ['axona_raw']
     entities_to_test = files_to_download
 
 
