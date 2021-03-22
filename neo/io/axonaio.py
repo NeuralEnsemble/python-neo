14ed2f17914ff24384101d6e1d0d62b75a6b8501
sbuergers <sbuergers@gmail.com>
2021-03-22 08:42:09 -0400

    removed variable assignment that already has it as default val

--- neo/io/axonaio.py
+++ neo/io/axonaio.py
@@ -6,11 +6,6 @@ class AxonaIO(AxonaRawIO, BaseFromRaw):
     name = 'Axona IO'
     description = "Read raw continuous data (.bin and .set files)"
 
-    # This is an inportant choice when there are several channels.
-    #   'split-all' :  1 AnalogSignal each 1 channel
-    #   'group-by-same-units' : one 2D AnalogSignal for each group of channel with same units
-    _prefered_signal_group_mode = 'group-by-same-units'
-
     def __init__(self, filename):
         AxonaRawIO.__init__(self, filename=filename)
         BaseFromRaw.__init__(self, filename)
