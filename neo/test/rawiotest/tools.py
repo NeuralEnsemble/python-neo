8e18c69f7ab057cb3341863504ec0317c0e25eab
Samuel Garcia <sam.garcia.die@gmail.com>
2021-04-22 18:13:11 +0200

    Fix travis when no datalad.

--- neo/test/rawiotest/tools.py
+++ neo/test/rawiotest/tools.py
@@ -10,11 +10,15 @@ from urllib.request import urlopen
 
 logger = logging.getLogger("neo.test")
 
+from neo.utils import HAVE_DATALAD
+
 
 def can_use_network():
     """
     Return True if network access is allowed
     """
+    if not HAVE_DATALAD:
+        return False
     if os.environ.get('NOSETESTS_NO_NETWORK', False):
         return False
     if os.environ.get('TRAVIS') == 'true':
