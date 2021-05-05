4fdb9cf8e64bdf2648be67f6becabb83c3af935c
Samuel Garcia <sam.garcia.die@gmail.com>
2021-05-05 17:07:13 +0200

    Add maxwell files.

--- neo/test/iotest/test_maxwellio.py
+++ neo/test/iotest/test_maxwellio.py
@@ -6,8 +6,13 @@ from neo.test.iotest.common_io_test import BaseTestIO
 
 class TestMaxwellIO(BaseTestIO, unittest.TestCase, ):
     ioclass = MaxwellIO
-    files_to_test = []
-    files_to_download = files_to_test
+    entities_to_download = [
+        'maxwell'
+    ]
+    entities_to_test = files_to_test = [
+        'maxwell/MaxOne_data/Record/000011/data.raw.h5',
+        'maxwell/MaxTwo_data/Network/000028/data.raw.h5'
+    ]
 
 
 if __name__ == "__main__":
--- neo/test/rawiotest/test_maxwellrawio.py
+++ neo/test/rawiotest/test_maxwellrawio.py
@@ -6,8 +6,13 @@ from neo.test.rawiotest.common_rawio_test import BaseTestRawIO
 
 class TestMaxwellRawIO(BaseTestRawIO, unittest.TestCase, ):
     rawioclass = MaxwellRawIO
-    entities_to_test = []
-    files_to_download = entities_to_test
+    entities_to_download = [
+        'maxwell'
+    ]
+    entities_to_test = files_to_test = [
+        'maxwell/MaxOne_data/Record/000011/data.raw.h5',
+        'maxwell/MaxTwo_data/Network/000028/data.raw.h5'
+    ]
 
 
 if __name__ == "__main__":
