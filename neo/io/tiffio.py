ada03a0fd621722e389964e2aaab4ade0bee340f
sprenger <julia.sprenger@univ-amu.fr>
2021-07-01 18:04:02 +0200

    fix failing array generation in some numpy versions

--- neo/io/tiffio.py
+++ neo/io/tiffio.py
@@ -102,8 +102,8 @@ class TiffIO(BaseIO):
         file_name_list = natural_sort(file_name_list)
         list_data_image = []
         for file_name in file_name_list:
-            list_data_image.append(
-                np.array(Image.open(self.filename + "/" + file_name), dtype=np.float32))
+            data = np.array(Image.open(self.filename + "/" + file_name)).astype(np.float32)
+            list_data_image.append(data)
         list_data_image = np.array(list_data_image)
         if len(list_data_image.shape) == 4:
             list_data_image = []
