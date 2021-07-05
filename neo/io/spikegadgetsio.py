45cbd44c78558ac1f0b5c59f39fc24600e15491f
Samuel Garcia <sam.garcia.die@gmail.com>
2021-07-05 09:41:16 +0200

    doc at neo.io for spikegadget

--- neo/io/spikegadgetsio.py
+++ neo/io/spikegadgetsio.py
@@ -3,8 +3,7 @@ from neo.rawio.spikegadgetsrawio import SpikeGadgetsRawIO
 
 
 class SpikeGadgetsIO(SpikeGadgetsRawIO, BaseFromRaw):
-    """
-    """
+    __doc__ = SpikeGadgetsRawIO.__doc__
     def __init__(self, filename):
         SpikeGadgetsRawIO.__init__(self, filename=filename)
         BaseFromRaw.__init__(self, filename)
