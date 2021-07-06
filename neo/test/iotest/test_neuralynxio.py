a41c1ef1be6a56fb54f5b451f587c6ee1bf180de
sprenger <julia.sprenger@univ-amu.fr>
2021-07-06 12:12:22 +0200

    [neuralynx] reintroduce ncs data test

--- neo/test/iotest/test_neuralynxio.py
+++ neo/test/iotest/test_neuralynxio.py
@@ -2,7 +2,7 @@
 Tests of neo.io.neuralynxio.py
 """
 
-import time
+import os
 import warnings
 
 import unittest
@@ -233,28 +233,32 @@ class TestData(CommonNeuralynxIOTest, unittest.TestCase):
 
             return [item for sublist in res for item in sublist]
 
-    # def test_ncs(self):
-        # for session in self.files_to_test:
-        #     dirname = self.get_local_path(session)
-        #     nio = NeuralynxIO(dirname=dirname, use_cache=False)
-        #     block = nio.read_block()
+    def test_ncs(self):
+        for session in self.files_to_test:
+            dirname = self.get_local_path(session)
+            nio = NeuralynxIO(dirname=dirname, use_cache=False)
+            block = nio.read_block()
 
-            # check that data agrees in first segment only
-            # for anasig_id, anasig in enumerate(block.segments[0].analogsignals):
-            #     chid = anasig.channel_index.channel_ids[anasig_id]
-            #
-            #     # need to decode, unless keyerror
-            #     chname = anasig.channel_index.channel_names[anasig_id]
-            #     chuid = (chname, chid)
-            #     filename = nio.ncs_filenames[chuid][:-3] + 'txt'
-            #     filename = filename.replace('original_data', 'plain_data')
-            #     overlap = 512 * 500
-            #     plain_data = self._load_plaindata(filename, overlap)
-            #     gain_factor_0 = plain_data[0] / anasig.magnitude[0, 0]
-            #     numToTest = min(len(plain_data), len(anasig.magnitude[:, 0]))
-            #     np.testing.assert_allclose(plain_data[:numToTest],
-            #                                anasig.magnitude[:numToTest, 0] * gain_factor_0,
-            #                                rtol=0.01, err_msg=" for file " + filename)
+            #check that data agrees in first segment first channel only
+            for anasig_id, anasig in enumerate(block.segments[0].analogsignals):
+                chid = int(anasig.array_annotations['channel_ids'][0])
+
+                chname = str(anasig.array_annotations['channel_names'][0])
+                chuid = (chname, chid)
+                filename = nio.ncs_filenames[chuid][:-3] + 'txt'
+                filename = filename.replace('original_data', 'plain_data')
+                overlap = 512 * 500
+                if os.path.isfile(filename):
+                    plain_data = self._load_plaindata(filename, overlap)
+                    gain_factor_0 = plain_data[0] / anasig.magnitude[0, 0]
+                    numToTest = min(len(plain_data), len(anasig.magnitude[:, 0]))
+                    np.testing.assert_allclose(plain_data[:numToTest],
+                                               anasig.magnitude[:numToTest, 0] * gain_factor_0,
+                                               rtol=0.01, err_msg=" for file " + filename)
+                else:
+                    warnings.warn(f'Could not find corresponding test file {filename}')
+                    # TODO: Create missing plain data file using NeuraView
+                    # https://neuralynx.com/software/category/data-analysis
 
     def test_keep_original_spike_times(self):
         for session in self.files_to_test:
