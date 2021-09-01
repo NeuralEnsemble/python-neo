025c273828ae31ebdccbd8e496f81eefded6445d
sprenger <julia.sprenger@univ-amu.fr>
2021-09-01 15:58:08 +0200

    [debug] fix dtype to int64 to avoid windows using int32 by default

--- neo/rawio/neuralynxrawio/ncssections.py
+++ neo/rawio/neuralynxrawio/ncssections.py
@@ -281,7 +281,7 @@ class NcsSectionsFactory:
         # detect records with incomplete number of samples
         gap_rec_ids = list(np.where(ncsMemMap['nb_valid'] != exp_nb_valid)[0])
 
-        pred_times = np.rint(ncsMemMap['timestamp'] + 1e6 / ncsSects.sampFreqUsed * ncsMemMap['nb_valid']).astype(int)
+        pred_times = np.rint(ncsMemMap['timestamp'] + 1e6 / ncsSects.sampFreqUsed * ncsMemMap['nb_valid']).astype(np.int64)
         max_pred_times = pred_times + maxGapLen
         # data records that start later than the predicted time (including the
         # maximal accepted gap length) are considered delayed a gap is
