cdfc3a1a3d6149e242afa87e74e737c73407e4a8
sprenger <julia.sprenger@univ-amu.fr>
2021-09-25 13:16:39 +0200

    [MLIO] update test

--- neo/test/iotest/test_monkeylogicio.py
+++ neo/test/iotest/test_monkeylogicio.py
@@ -21,12 +21,18 @@ class TestMonkeyLogicIO(unittest.TestCase):
     # TODO: Adjust this once ML files are on GIN
 
     def test_read(self):
-        filename = '/home/sprengerj/projects/monkey_logic/210909_TSCM_5cj_5cl_Riesling.bhv2'
-        filename = '/home/sprengerj/projects/monkey_logic/sabrina/210810__learndms_userloop.bhv2'
+        filename = '/home/sprengerj/projects/monkey_logic/guilhem/210909_TSCM_5cj_5cl_Riesling.bhv2'
+        # filename = '/home/sprengerj/projects/monkey_logic/sabrina/210810__learndms_userloop.bhv2'
         # filename = '/home/sprengerj/projects/monkey_logic/sabrina/210916__learndms_userloop.bhv2'
         # filename = '/home/sprengerj/projects/monkey_logic/sabrina/210917__learndms_userloop.bhv2'
         io = MonkeyLogicIO(filename)
-        io.read_block()
+        bl = io.read_block()
+
+        assert len(bl.segments) == len(io.trial_ids)
+        assert 'Trial' in bl.segments[0].annotations
+        assert len(bl.segments[0].events) == 1
+        print(bl.segments[0].events[0].times)
+
 
 if __name__ == "__main__":
     unittest.main()
