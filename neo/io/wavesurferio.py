c2f03b3247ec7a88c5b1787e04424f3dc1d8237c
Joe Z <Joseph.J.Ziminski@gmail.com>
2021-10-10 16:13:54 +0100

    added wavesurferio to io __init__

---
+++ neo/io/wavesurferio.py
@@ -0,0 +1,29 @@
+"""
+neo.io have been split in 2 level API:
+  * neo.io: this API give neo object
+  * neo.rawio: this API give raw data as they are in files.
+
+Developper are encourage to use neo.rawio.
+
+When this is done the neo.io is done automagically with
+this king of following code.
+
+Author: sgarcia
+
+"""
+
+from neo.io.basefromrawio import BaseFromRaw
+from neo.rawio.wavesurferrawio import WaveSurferRawIO
+
+class WaveSurferIO(WaveSurferRawIO, BaseFromRaw):
+    name = 'example IO'
+    description = "Fake IO"
+
+    # This is an inportant choice when there are several channels.
+    #   'split-all' :  1 AnalogSignal each 1 channel
+    #   'group-by-same-units' : one 2D AnalogSignal for each group of channel with same units
+    _prefered_signal_group_mode = "split-all"
+
+    def __init__(self, filename=''):
+        WaveSurferRawIO.__init__(self, filename=filename)
+        BaseFromRaw.__init__(self, filename)
