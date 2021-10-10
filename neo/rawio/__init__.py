cf6520035e210d22e09c2e3005a35f18cd2a1baa
Joe Z <Joseph.J.Ziminski@gmail.com>
2021-10-10 16:07:57 +0100

    added wavesurferrawio to rawio __init__

--- neo/rawio/__init__.py
+++ neo/rawio/__init__.py
@@ -42,6 +42,7 @@ Classes:
 * :attr:`SpikeGadgetsRawIO`
 * :attr:`SpikeGLXRawIO`
 * :attr:`TdtRawIO`
+* :attr:`WaveSurferRawIO`
 * :attr:`WinEdrRawIO`
 * :attr:`WinWcpRawIO`
 
@@ -166,6 +167,10 @@ Classes:
 
     .. autoattribute:: extensions
 
+.. autoclass:: neo.rawio.WaveSurferRawIO
+
+    .. autoattribute:: extensions
+
 .. autoclass:: neo.rawio.WinEdrRawIO
 
     .. autoattribute:: extensions
@@ -209,6 +214,7 @@ from neo.rawio.spike2rawio import Spike2RawIO
 from neo.rawio.spikegadgetsrawio import SpikeGadgetsRawIO
 from neo.rawio.spikeglxrawio import SpikeGLXRawIO
 from neo.rawio.tdtrawio import TdtRawIO
+from neo.rawio.wavesurferrawio import WaveSurferRawIO
 from neo.rawio.winedrrawio import WinEdrRawIO
 from neo.rawio.winwcprawio import WinWcpRawIO
 
@@ -243,6 +249,7 @@ rawiolist = [
     SpikeGadgetsRawIO,
     SpikeGLXRawIO,
     TdtRawIO,
+    WaveSurferRawIO,
     WinEdrRawIO,
     WinWcpRawIO,
 ]
