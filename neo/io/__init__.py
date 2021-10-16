ec63537b83cec6dfbe7ff24ec1699051b927df4c
Joe Z <Joseph.J.Ziminski@gmail.com>
2021-10-16 21:36:41 +0100

    update neo/io/__init__.py

--- neo/io/__init__.py
+++ neo/io/__init__.py
@@ -64,6 +64,7 @@ Classes:
 * :attr:`StimfitIO`
 * :attr:`TdtIO`
 * :attr:`TiffIO`
+* :attr:`WaveSurferIO`
 * :attr:`WinEdrIO`
 * :attr:`WinWcpIO`
 
@@ -268,6 +269,10 @@ Classes:
 
     .. autoattribute:: extensions
 
+.. autoclass:: neo.io.WaveSurferIO
+
+    .. autoattribute:: extensions
+
 .. autoclass:: neo.io.WinEdrIO
 
     .. autoattribute:: extensions
@@ -350,6 +355,7 @@ from neo.io.spikeglxio import SpikeGLXIO
 from neo.io.stimfitio import StimfitIO
 from neo.io.tdtio import TdtIO
 from neo.io.tiffio import TiffIO
+from neo.io.wavesurferio import WaveSurferIO
 from neo.io.winedrio import WinEdrIO
 from neo.io.winwcpio import WinWcpIO
 
@@ -406,6 +412,7 @@ iolist = [
     StimfitIO,
     TdtIO,
     TiffIO,
+    WaveSurferIO,
     WinEdrIO,
     WinWcpIO,
 ]
