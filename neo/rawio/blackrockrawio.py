b80fd31164579cc4a1fcbd1aaadec903c2851b61
Fernando J. Chaure <ferchaure@users.noreply.github.com>
2022-08-13 12:50:03 -0300

    improves blackrock spikes in nev

--- neo/rawio/blackrockrawio.py
+++ neo/rawio/blackrockrawio.py
@@ -412,7 +412,7 @@ class BlackrockRawIO(BaseRawIO):
                     for k, (data, ev_ids) in self.nev_data.items():
                         segment_mask = ev_ids == data_bl
                         if data[segment_mask].size > 0:
-                            t = data[segment_mask][-1]['timestamp'] / self.__nev_basic_header[
+                            t = (data[segment_mask][-1]['timestamp']+1) / self.__nev_basic_header[
                                 'timestamp_resolution']
                             max_nev_time = max(max_nev_time, t)
                     if max_nev_time > t_stop:
@@ -450,7 +450,7 @@ class BlackrockRawIO(BaseRawIO):
             # Calculate t_start and t_stop for each segment in seconds
             resolution = self.__nev_basic_header['timestamp_resolution']
             self._seg_t_starts = [v / float(resolution) for k, v in sorted(min_nev_times.items())]
-            self._seg_t_stops = [v / float(resolution) for k, v in sorted(max_nev_times.items())]
+            self._seg_t_stops = [(v+1) / float(resolution) for k, v in sorted(max_nev_times.items())]
             self._nb_segment = len(self._seg_t_starts)
             self._sigs_t_starts = [None] * self._nb_segment
 
@@ -645,8 +645,13 @@ class BlackrockRawIO(BaseRawIO):
 
         wf_dtype = self.__nev_params('waveform_dtypes')[channel_id]
         wf_size = self.__nev_params('waveform_size')[channel_id]
+        wf_byte_size = np.dtype(wf_dtype).itemsize * wf_size
 
-        waveforms = unit_spikes['waveform'].flatten().view(wf_dtype)
+        dt1 = [
+            ('extra', 'S{}'.format(unit_spikes['waveform'].dtype.itemsize-wf_byte_size)),
+            ('ch_waveform', 'S{}'.format(wf_byte_size))]
+
+        waveforms = unit_spikes['waveform'].view(dt1)['ch_waveform'].flatten().view(wf_dtype)
         waveforms = waveforms.reshape(int(unit_spikes.size), 1, int(wf_size))
 
         timestamp = unit_spikes['timestamp']
