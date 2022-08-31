b9389a54183e37d8b0d17f74c4655dfb51bf2053
Alessio Buccino <alejoe9187@gmail.com>
2022-08-31 11:06:09 +0200

    Add new OE test folder

--- neo/test/rawiotest/test_openephysbinaryrawio.py
+++ neo/test/rawiotest/test_openephysbinaryrawio.py
@@ -13,6 +13,7 @@ class TestOpenEphysBinaryRawIO(BaseTestRawIO, unittest.TestCase):
         'openephysbinary/v0.5.3_two_neuropixels_stream',
         'openephysbinary/v0.4.4.1_with_video_tracking',
         'openephysbinary/v0.5.x_two_nodes',
+        'openephysbinary/v0.6.x_neuropixels_multiexp_multistream',
     ]
 
 
