58fd97825c6ad5a75b0eabced79b4c70ffaa1057
Samuel Garcia <sam.garcia.die@gmail.com>
2022-10-05 21:16:43 +0200

    Fix init with version at begining

--- neo/__init__.py
+++ neo/__init__.py
@@ -2,6 +2,10 @@
 Neo is a package for representing electrophysiology data in Python,
 together with support for reading a wide range of neurophysiology file formats
 '''
+import importlib.metadata
+# this need to be at the begining because some sub module will need the version
+__version__ = importlib.metadata.version("neo")
+
 import logging
 
 logging_handler = logging.StreamHandler()
@@ -9,5 +13,3 @@ logging_handler = logging.StreamHandler()
 from neo.core import *
 from neo.io import *
 
-import importlib.metadata
-__version__ = importlib.metadata.version("neo")
