5a3fb22c9abb93ef575869fe2ff615a56a198e21
Samuel Garcia <sam.garcia.die@gmail.com>
2022-10-05 11:40:11 +0200

    Draft to move from setup.py to pyproject.toml

--- neo/__init__.py
+++ neo/__init__.py
@@ -2,6 +2,8 @@
 Neo is a package for representing electrophysiology data in Python,
 together with support for reading a wide range of neurophysiology file formats
 '''
+import importlib.metadata
+__version__ = importlib.metadata.version("neo")
 
 import logging
 
@@ -9,5 +11,3 @@ logging_handler = logging.StreamHandler()
 
 from neo.core import *
 from neo.io import *
-
-from neo.version import version as __version__
