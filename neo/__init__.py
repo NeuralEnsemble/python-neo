6bd93c62aa8947d0429694be1d8a534462fd6520
Samuel Garcia <sam.garcia.die@gmail.com>
2022-10-05 13:53:18 +0200

    Adress some andrew's comments.

--- neo/__init__.py
+++ neo/__init__.py
@@ -2,12 +2,12 @@
 Neo is a package for representing electrophysiology data in Python,
 together with support for reading a wide range of neurophysiology file formats
 '''
-import importlib.metadata
-__version__ = importlib.metadata.version("neo")
-
 import logging
 
 logging_handler = logging.StreamHandler()
 
 from neo.core import *
 from neo.io import *
+
+import importlib.metadata
+__version__ = importlib.metadata.version("neo")
