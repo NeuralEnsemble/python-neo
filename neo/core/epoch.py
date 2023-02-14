311a04f442545bd0834f1b9534cccb6f828e2a7d
Julia Sprenger <julia.sprenger@rwth-aachen.de>
2023-02-14 11:11:27 +0100

    Make rescale signature compatible with quantities 0.14.0

--- neo/core/epoch.py
+++ neo/core/epoch.py
@@ -184,10 +184,11 @@ class Epoch(DataObject):
             pp.breakable()
             self._repr_pretty_attrs_(pp, cycle)
 
-    def rescale(self, units):
+    def rescale(self, units, dtype=None):
         '''
-        Return a copy of the :class:`Epoch` converted to the specified
-        units
+        Return a copy of the :class:`Epoch` converted to the specified units
+        The `dtype` argument exists only for backward compatibility within quantities, see
+        https://github.com/python-quantities/python-quantities/pull/204
         :return: Copy of self with specified units
         '''
         # Use simpler functionality, if nothing will be changed
