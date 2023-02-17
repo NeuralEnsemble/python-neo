d550b80f08fd28570284413397ed3698b86f704e
Andrew Davison <andrew.davison@cnrs.fr>
2023-02-17 13:20:38 +0100

    Update documentation to reflect change to pyproject.toml

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -50,7 +50,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Neo'
-copyright = '2010-2022, ' + AUTHORS
+copyright = '2010-2023, ' + AUTHORS
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
