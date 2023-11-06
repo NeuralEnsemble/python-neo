3e3392c19744ea7d4ddddaeb8da84c6749a66f4c
Alessio Buccino <alejoe9187@gmail.com>
2023-11-06 12:12:12 +0100

    Revert maxwell changes

--- neo/test/iotest/test_maxwellio.py
+++ neo/test/iotest/test_maxwellio.py
@@ -6,8 +6,6 @@ from neo.test.iotest.common_io_test import BaseTestIO
 
 from neo.rawio.maxwellrawio import auto_install_maxwell_hdf5_compression_plugin
 
-ON_GITHUB = bool(os.getenv('GITHUB_ACTIONS'))
-
 
 class TestMaxwellIO(BaseTestIO, unittest.TestCase, ):
     ioclass = MaxwellIO
@@ -20,14 +18,7 @@ class TestMaxwellIO(BaseTestIO, unittest.TestCase, ):
     ]
 
     def setUp(self):
-        if ON_GITHUB:
-            # set a custom existing path for the hdf5 plugin
-            hdf5_plugin_path = '/home/runner/work/hdf5_plugin_path_maxwell'
-            os.environ['HDF5_PLUGIN_PATH'] = hdf5_plugin_path
-        else:
-            hdf5_plugin_path = None
-        auto_install_maxwell_hdf5_compression_plugin(hdf5_plugin_path=hdf5_plugin_path,
-                                                     force_download=False)
+        auto_install_maxwell_hdf5_compression_plugin(force_download=False)
         BaseTestIO.setUp(self)
 
 
--- neo/test/rawiotest/test_maxwellrawio.py
+++ neo/test/rawiotest/test_maxwellrawio.py
@@ -5,7 +5,6 @@ from neo.rawio.maxwellrawio import (MaxwellRawIO,
     auto_install_maxwell_hdf5_compression_plugin)
 from neo.test.rawiotest.common_rawio_test import BaseTestRawIO
 
-ON_GITHUB = bool(os.getenv('GITHUB_ACTIONS'))
 
 class TestMaxwellRawIO(BaseTestRawIO, unittest.TestCase, ):
     rawioclass = MaxwellRawIO
@@ -18,14 +17,7 @@ class TestMaxwellRawIO(BaseTestRawIO, unittest.TestCase, ):
     ]
 
     def setUp(self):
-        if ON_GITHUB:
-            # set a custom existing path for the hdf5 plugin
-            hdf5_plugin_path = '/home/runner/work/hdf5_plugin_path_maxwell'
-            os.environ['HDF5_PLUGIN_PATH'] = hdf5_plugin_path
-        else:
-            hdf5_plugin_path = None
-        auto_install_maxwell_hdf5_compression_plugin(hdf5_plugin_path=hdf5_plugin_path,
-                                                     force_download=False)
+        auto_install_maxwell_hdf5_compression_plugin(force_download=False)
         BaseTestRawIO.setUp(self)
 
 
