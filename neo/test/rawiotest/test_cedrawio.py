606659a0f8a1c30a4cfae97b54057d0b44bfd140
Andrew Davison <andrew.davison@cnrs.fr>
2023-12-08 14:03:29 +0100

    Fix some tests that fail when running locally.
    In most cases, this is because a dependency isn't installed, in this case the tests are now skipped rather than failing.

--- neo/test/rawiotest/test_cedrawio.py
+++ neo/test/rawiotest/test_cedrawio.py
@@ -4,6 +4,15 @@ from neo.rawio.cedrawio import CedRawIO
 
 from neo.test.rawiotest.common_rawio_test import BaseTestRawIO
 
+try:
+    import sonpy
+    HAVE_SONPY = True
+except ImportError:
+    HAVE_SONPY = False
+
+
+# This runs standard tests, this is mandatory for all IOs
+@unittest.skipUnless(HAVE_SONPY, "requires sonpy package and all its dependencies")
 
 class TestCedRawIO(BaseTestRawIO, unittest.TestCase, ):
     rawioclass = CedRawIO
