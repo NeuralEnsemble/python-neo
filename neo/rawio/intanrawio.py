32d2685faaefe278ed574e7c9fe86adc95029729
Heberto Mayorquin <h.mayorquin@gmail.com>
2024-02-23 12:46:50 -0600

    change channel id to native-channel-name in intan
    use channel ids for accessing memmaps

--- neo/rawio/intanrawio.py
+++ neo/rawio/intanrawio.py
@@ -85,8 +85,8 @@ class IntanRawIO(BaseRawIO):
         # signals
         signal_channels = []
         for c, chan_info in enumerate(self._ordered_channels):
-            name = chan_info["native_channel_name"]
-            chan_id = str(c)  # the chan_id have no meaning in intan
+            name = chan_info["custom_channel_name"]
+            chan_id = chan_info["native_channel_name"]
             if chan_info["signal_type"] == 20:
                 # exception for temperature
                 sig_dtype = "int16"
@@ -146,9 +146,9 @@ class IntanRawIO(BaseRawIO):
         stream_id = self.header["signal_streams"][stream_index]["id"]
         mask = self.header["signal_channels"]["stream_id"] == stream_id
         signal_channels = self.header["signal_channels"][mask]
-        channel_names = signal_channels["name"]
-        chan_name0 = channel_names[0]
-        size = self._raw_data[chan_name0].size
+        channel_ids = signal_channels["id"]
+        channel_id_0 = channel_ids[0]
+        size = self._raw_data[channel_id_0].size
         return size
 
     def _get_signal_t_start(self, block_index, seg_index, stream_index):
@@ -166,9 +166,9 @@ class IntanRawIO(BaseRawIO):
         signal_channels = self.header["signal_channels"][mask]
         if channel_indexes is None:
             channel_indexes = slice(None)
-        channel_names = signal_channels["name"][channel_indexes]
+        channel_ids = signal_channels["id"][channel_indexes]
 
-        shape = self._raw_data[channel_names[0]].shape
+        shape = self._raw_data[channel_ids[0]].shape
 
         # some channel (temperature) have 1D field so shape 1D
         # because 1 sample per block
@@ -181,13 +181,14 @@ class IntanRawIO(BaseRawIO):
             sl0 = i_start % block_size
             sl1 = sl0 + (i_stop - i_start)
 
-        sigs_chunk = np.zeros((i_stop - i_start, len(channel_names)), dtype="uint16")
-        for i, chan_name in enumerate(channel_names):
-            data_chan = self._raw_data[chan_name]
+        sigs_chunk = np.zeros((i_stop - i_start, len(channel_ids)), dtype="uint16")
+        for channel_index, channel_id in enumerate(channel_ids):
+            # Memmap fields are the channel_ids for unique channels
+            data_chan = self._raw_data[channel_id]
             if len(shape) == 1:
-                sigs_chunk[:, i] = data_chan[i_start:i_stop]
+                sigs_chunk[:, channel_index] = data_chan[i_start:i_stop]
             else:
-                sigs_chunk[:, i] = data_chan[block_start:block_stop].flatten()[sl0:sl1]
+                sigs_chunk[:, channel_index] = data_chan[block_start:block_stop].flatten()[sl0:sl1]
 
         return sigs_chunk
 
@@ -448,7 +449,10 @@ def read_rhd(filename):
 
         global_info = read_variable_header(f, rhd_global_header_base)
 
-        version = V("{major_version}.{minor_version}".format(**global_info))
+        # This is a package.version object that allows lexicographic comparison
+        major_version = global_info["major_version"]
+        minor_version = global_info["minor_version"]
+        version = V(f"{major_version}.{minor_version}")
 
         # the header size depends on the version :-(
         header = list(rhd_global_header_part1)  # make a copy
