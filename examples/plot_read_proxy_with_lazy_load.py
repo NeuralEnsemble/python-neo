fix examples displaying wrong order

examples/plot_read_proxy_with_lazy_load.py
@@ -4,6 +4,9 @@ Demonstration of lazy load and proxy objects
 
 """
 
+################################################
+# Import our packages first
+
 import urllib
 import neo
 import quantities as pq
@@ -11,18 +14,27 @@ import numpy as np
 
 url_repo = "https://web.gin.g-node.org/NeuralEnsemble/ephy_testing_data/raw/master/"
 
-# Get Plexon files
+
+###############################################
+# Let's get a file
+
+# Get med file
 distantfile = url_repo + "micromed/File_micromed_1.TRC"
 localfile = "./File_micromed_1.TRC"
 urllib.request.urlretrieve(distantfile, localfile)
 
+##################################################
 # create a reader
+
 reader = neo.MicromedIO(filename="File_micromed_1.TRC")
 reader.parse_header()
 
 
 lim0, lim1 = -20 * pq.ms, +20 * pq.ms
 
+#####################################################
+# Now let's make a function that we want to apply to
+# look at lazy for eager uses of the API
 
 def apply_my_fancy_average(sig_list):
     """basic average along triggers and then channels
@@ -33,6 +45,9 @@ def apply_my_fancy_average(sig_list):
     sigs = np.stack(sig_list, axis=0)
     return np.mean(np.mean(sigs, axis=0), axis=1)
 
+##################################################
+# We start with eager where `lazy=False`. Everything
+# is loaded into memory
 
 seg = reader.read_segment(lazy=False)
 triggers = seg.events[0]
@@ -44,6 +59,11 @@ for t in triggers.times:
     all_sig_chunks.append(anasig_chunk)
 m1 = apply_my_fancy_average(all_sig_chunks)
 
+#####################################################
+# Here we do `lazy=True`, so we do lazy loading. We
+# only load the events that we want into memory
+# and we use a proxy object for our analogsignal until we
+# load it chunk by chunk (no running out of memory!)
 
 seg = reader.read_segment(lazy=True)
 triggers = seg.events[0].load(time_slice=None)  # this load all trigers in memory
@@ -55,5 +75,8 @@ for t in triggers.times:
     all_sig_chunks.append(anasig_chunk)
 m2 = apply_my_fancy_average(all_sig_chunks)
 
+##########################################################
+# We see that either way the result is the same, but 
+# we do not exhaust our RAM/memory
 print(m1)
 print(m2)

examples/plot_roi_demo.py
@@ -3,6 +3,8 @@ Working with RegionOfInterest objects
 =====================================
 
 """
+#############################################
+# Import our packages
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -11,7 +13,7 @@ from numpy.random import rand
 import random
 import quantities as pq
 
-
+##################################################################
 # First we create our image_sequence. Let's generate some data
 
 l = []
@@ -24,6 +26,8 @@ for frame in range(50):
 
 image_seq = ImageSequence(l, sampling_rate=500 * pq.Hz, spatial_scale="m", units="V")
 
+#################################################################
+# Now we will write a function for plotting an roi
 def plot_roi(roi, shape):
     img = rand(120, 100)
     pir = np.array(roi.pixels_in_region()).T
@@ -35,6 +39,8 @@ def plot_roi(roi, shape):
     ax = plt.gca()
     ax.add_artist(shape)
 
+##################################################################
+# Finally we will plot each roi
 
 roi = CircularRegionOfInterest(image_sequence=image_seq, x=50.3, y=50.8, radius=30.2)
 shape = plt.Circle(roi.centre, roi.radius, color="r", fill=False)