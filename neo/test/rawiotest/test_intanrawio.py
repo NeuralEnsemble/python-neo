fca20433b0a649d53acf2b9503a459293362e8fe
Heberto Mayorquin <h.mayorquin@gmail.com>
2024-05-14 16:42:03 -0600

    some test

--- neo/test/rawiotest/test_intanrawio.py
+++ neo/test/rawiotest/test_intanrawio.py
@@ -21,5 +21,26 @@ class TestIntanRawIO(
     ]
 
 
+    def test_annotations(self):
+        
+        intan_reader = IntanRawIO(filename=self.get_local_path("intan/intan_rhd_test_1.rhd"))
+        intan_reader.parse_header()
+        
+        raw_annotations = intan_reader.raw_annotations
+        annotations = raw_annotations["blocks"][0]["segments"][0]  # Intan is mono segment
+        signal_annotations = annotations["signals"][0]  # As in the other exmaples, annotaions are duplicated
+        
+        
+        exepcted_annotations =  {'intan_version': '1.5', 'desired_impedance_test_frequency': 1000.0, 'desired_upper_bandwidth': 7500.0, 'note1': '', 'notch_filter_mode': 1, 'notch_filter': False, 'nb_signal_group': 7, 
+                            'dsp_enabled': 1, 'actual_impedance_test_frequency': 1000.0, 'desired_lower_bandwidth': 0.1, 'note3': '', 'actual_dsp_cutoff_frequency': 1.165828, 
+                            'desired_dsp_cutoff_frequency': 1.0, 'actual_lower_bandwidth': 0.0945291, 'eval_board_mode': 0, 'note2': '', 'num_temp_sensor_channels': 0}
+        
+        for key in exepcted_annotations:
+            if isinstance(exepcted_annotations[key], float):
+                self.assertAlmostEqual(signal_annotations[key], exepcted_annotations[key], places=2)
+            else:
+                self.assertEqual(signal_annotations[key], exepcted_annotations[key])
+
+   
 if __name__ == "__main__":
     unittest.main()
