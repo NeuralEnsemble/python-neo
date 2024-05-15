ecec2717e080f231c0d7b29369a9df26a45ee71f
Heberto Mayorquin <h.mayorquin@gmail.com>
2024-05-15 08:13:23 -0600

    add test for header attached anyway

--- neo/test/iotest/test_intanio.py
+++ neo/test/iotest/test_intanio.py
@@ -16,13 +16,13 @@ class TestIntanIO(
     entities_to_download = ["intan"]
     entities_to_test = [
         "intan/intan_rhs_test_1.rhs",  # Format header-attached 
-        "intan/intan_rhd_test_1.rhd",  # Format header attach
+        "intan/intan_rhd_test_1.rhd",  # Format header-attached
+        "intan/rhs_fpc_multistim_240514_082243/rhs_fpc_multistim_240514_082243.rhs",  # Format header-attached newer version
         "intan/intan_fpc_test_231117_052630/info.rhd",  # Format one-file-per-channel
         "intan/intan_fps_test_231117_052500/info.rhd",  # Format one file per signal
         "intan/intan_fpc_rhs_test_240329_091637/info.rhs",  # Format one-file-per-channel
         "intan/intan_fps_rhs_test_240329_091536/info.rhs",   # Format one-file-per-signal
-        "intan/rhd_fpc_multistim_240514_082044/info.rhd",  # Multiple digital channels one-file-per-channel
-        # "intan/rhs_fpc_multistim_240514_082243/info.rhs",  # Multiple digital channels one-file-per-channel rhs
+        "intan/rhd_fpc_multistim_240514_082044/info.rhd",  # Multiple digital channels one-file-per-channel rhd
     ]
 
 
