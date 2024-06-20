NeuralEnsemble/python-neo

commit baba820f2a7830bccd7f0672e1ef1a2cb9474da9

Allowing a datetime2format as required for Pegasus v2.2.2.
Hopefully also clearing up the lint with end commas that black was complaining about.

neo/rawio/neuralynxrawio/nlxheader.py
@@ -81,51 +81,52 @@ class NlxHeader(OrderedDict):
         "bml": dict(
             datetime1_regex=r"## Time Opened: \(m/d/y\): (?P<date>\S+)" r"  At Time: (?P<time>\S+)",
             filename_regex=r"## File Name: (?P<filename>\S+)",
-            datetimeformat="%m/%d/%y %H:%M:%S.%f",
+            datetimeformat="%m/%d/%y %H:%M:%S.%f"
         ),
         # Cheetah after version 1 and before version 5
         "bv5": dict(
             datetime1_regex=r"## Time Opened: \(m/d/y\): (?P<date>\S+)" r"  At Time: (?P<time>\S+)",
             filename_regex=r"## File Name: (?P<filename>\S+)",
-            datetimeformat="%m/%d/%Y %H:%M:%S.%f",
+            datetimeformat="%m/%d/%Y %H:%M:%S.%f"
         ),
         # Cheetah version 5.4.0
         "v5.4.0": dict(
             datetime1_regex=r"## Time Opened \(m/d/y\): (?P<date>\S+)" r"  At Time: (?P<time>\S+)",
             datetime2_regex=r"## Time Closed \(m/d/y\): (?P<date>\S+)" r"  At Time: (?P<time>\S+)",
             filename_regex=r"## File Name: (?P<filename>\S+)",
-            datetimeformat="%m/%d/%Y %H:%M:%S.%f",
+            datetimeformat="%m/%d/%Y %H:%M:%S.%f"
         ),
         # Cheetah version 5.6.0, some range of versions in between
         "v5.6.0": dict(
             datetime1_regex=r"## Time Opened: \(m/d/y\): (?P<date>\S+)" r" At Time: (?P<time>\S+)",
             filename_regex=r"## File Name: (?P<filename>\S+)",
-            datetimeformat="%m/%d/%Y %H:%M:%S.%f",
+            datetimeformat="%m/%d/%Y %H:%M:%S.%f"
         ),
         # Cheetah version 5 before and including v 5.6.4 as well as version 1
         "bv5.6.4": dict(
             datetime1_regex=r"## Time Opened \(m/d/y\): (?P<date>\S+)" r"  \(h:m:s\.ms\) (?P<time>\S+)",
             datetime2_regex=r"## Time Closed \(m/d/y\): (?P<date>\S+)" r"  \(h:m:s\.ms\) (?P<time>\S+)",
             filename_regex=r"## File Name (?P<filename>\S+)",
-            datetimeformat="%m/%d/%Y %H:%M:%S.%f",
+            datetimeformat="%m/%d/%Y %H:%M:%S.%f"
         ),
         "neuraview2": dict(
             datetime1_regex=r"## Date Opened: \(mm/dd/yyy\): (?P<date>\S+)" r" At Time: (?P<time>\S+)",
             datetime2_regex=r"## Date Closed: \(mm/dd/yyy\): (?P<date>\S+)" r" At Time: (?P<time>\S+)",
             filename_regex=r"## File Name: (?P<filename>\S+)",
-            datetimeformat="%m/%d/%Y %H:%M:%S",
+            datetimeformat="%m/%d/%Y %H:%M:%S"
         ),
         'peg': dict(
             datetime1_regex=r'-TimeCreated (?P<date>\S+) (?P<time>\S+)',
             datetime2_regex=r'-TimeClosed (?P<date>\S+) (?P<time>\S+)',
             filename_regex=r'-OriginalFileName "?(?P<filename>\S+)"?',
-            datetimeformat='%Y/%m/%d %H:%M:%S.%f'),
+            datetimeformat=r'%Y/%m/%d %H:%M:%S',
+            datetime2format=r'%Y/%m/%d %H:%M:%S.f'),
         # Cheetah after v 5.6.4 and default for others such as Pegasus
         "def": dict(
             datetime1_regex=r"-TimeCreated (?P<date>\S+) (?P<time>\S+)",
             datetime2_regex=r"-TimeClosed (?P<date>\S+) (?P<time>\S+)",
             filename_regex=r'-OriginalFileName "?(?P<filename>\S+)"?',
-            datetimeformat="%Y/%m/%d %H:%M:%S",
+            datetimeformat="%Y/%m/%d %H:%M:%S"
         ),
     }
 
@@ -263,9 +264,12 @@ class NlxHeader(OrderedDict):
             )
         else:
             dt1 = sr.groupdict()
-            self["recording_opened"] = datetime.datetime.strptime(
-                dt1["date"] + " " + dt1["time"], hpd["datetimeformat"]
-            )
+            try:  # allow two possible formats for date and time
+                self["recording_opened"] = datetime.datetime.strptime(
+                    dt1["date"] + " " + dt1["time"], hpd["datetimeformat"])
+            except:
+                self["recording_opened"] = datetime.datetime.strptime(
+                    dt1["date"] + " " + dt1["time"], hpd["datetime2format"])
 
         # close time, if available
         if "datetime2_regex" in hpd:

commit b8037d151c6056f9e0f151485de46232fe38fbf9

Fix: fix typo in datetime regex
Issue was introduced in commit baba820

neo/rawio/neuralynxrawio/nlxheader.py
@@ -120,7 +120,7 @@ class NlxHeader(OrderedDict):
             datetime2_regex=r"-TimeClosed (?P<date>\S+) (?P<time>\S+)",
             filename_regex=r'-OriginalFileName "?(?P<filename>\S+)"?',
             datetimeformat=r"%Y/%m/%d %H:%M:%S",
-            datetime2format=r"%Y/%m/%d %H:%M:%S.f",
+            datetime2format=r"%Y/%m/%d %H:%M:%S.%f",
         ),
         # Cheetah after v 5.6.4 and default for others such as Pegasus
         "def": dict(