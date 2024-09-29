d17e4f6afcdd983c12b2e75daef5688c5bb74bcc
apdavison <apdavison@users.noreply.github.com>
2024-09-29 12:13:41 +0000

    black formatting

--- neo/test/rawiotest/test_neuronexusrawio.py
+++ neo/test/rawiotest/test_neuronexusrawio.py
@@ -11,6 +11,4 @@ class TestNeuroNexusRawIO(
 ):
     rawioclass = NeuroNexusRawIO
     entities_to_download = ["neuronexus"]
-    entities_to_test = [
-        "neuronexus/allego_1/allego_2__uid0701-13-04-49.xdat.json"
-    ]
+    entities_to_test = ["neuronexus/allego_1/allego_2__uid0701-13-04-49.xdat.json"]
