7fd395187583e03806ce9ac5256fac657727d513
MureziCapaul <murezi.capaul@kliniklengg.ch>
2025-02-11 11:41:15 +0100

    Add basic tests

---
+++ neo/test/rawiotest/test_nicoletrawio.py
@@ -0,0 +1,18 @@
+import unittest
+
+from neo.rawio.nicoletrawio import NicoletRawIO
+
+from neo.test.rawiotest.common_rawio_test import BaseTestRawIO
+
+
+class TestExampleRawIO(
+    BaseTestRawIO,
+    unittest.TestCase,
+):
+    rawioclass = NicoletRawIO
+    entities_to_download = ['nicolet']
+
+    entities_to_test = ["nicolet/e_files/test_nicolet.e"]
+    
+if __name__ == "__main__":
+    unittest.main()
