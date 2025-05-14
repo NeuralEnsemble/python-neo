5df340368f264be757719ced01b3f96a49f3506d
Luiz Tauffer <luiz_tauffer@hotmail.com>
2025-05-14 14:48:32 +0200

    BlackRock: shape parameter for memap (#1705)
    * shape parameter for memap
    
    * Update neo/rawio/blackrockrawio.py
    
    Co-authored-by: Zach McKenzie <92116279+zm711@users.noreply.github.com>
    
    * Update neo/rawio/blackrockrawio.py
    
    ---------
    
    Co-authored-by: Zach McKenzie <92116279+zm711@users.noreply.github.com>

--- neo/rawio/blackrockrawio.py
+++ neo/rawio/blackrockrawio.py
@@ -1254,7 +1254,21 @@ class BlackrockRawIO(BaseRawIO):
         # read all raw data packets and markers
         dt0 = [("timestamp", ts_format), ("packet_id", "uint16"), ("value", f"S{data_size - header_skip}")]
 
-        raw_data = np.memmap(filename, offset=header_size, dtype=dt0, mode="r")
+        # expected number of data packets. We are not sure why, but it seems we can get partial data packets
+        # based on blackrock's own code this is okay so applying an int to round down is necessary to obtain the
+        # memory map of full packets and toss the partial packet.
+        # See reference: https://github.com/BlackrockNeurotech/Python-Utilities/blob/fa75aa671680306788e10d3d8dd625f9da4ea4f6/brpylib/brpylib.py#L580-L587
+        n_packets = int(
+            (self.__get_file_size(filename) - header_size) / data_size
+        )
+
+        raw_data = np.memmap(
+            filename,
+            offset=header_size,
+            dtype=dt0,
+            shape=(n_packets,),
+            mode="r",
+        )
 
         masks = self.__nev_data_masks(raw_data["packet_id"])
         types = self.__nev_data_types(data_size)
