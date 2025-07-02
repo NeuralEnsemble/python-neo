63a9d347c83b8b7904a91621d5629bfe570142e9
MureziCapaul <murezi.capaul@protonmail.ch>
2025-07-02 15:51:17 +0200

    Add test for every entitiy

--- neo/test/iotest/test_nicoletio.py
+++ neo/test/iotest/test_nicoletio.py
@@ -35,71 +35,52 @@ class TestNicoletIO(
         super().tearDown()
         for entity in self.entities_to_test:
             full_path = get_test_file_full_path(self.ioclass, filename=entity, directory=self.local_test_dir)
-            pathlib.Path(full_path).unlink(missing_ok=True)
+            #pathlib.Path(full_path).unlink(missing_ok=True)
 
-class TestNicoletIO(unittest.TestCase):
     def test_read_segment_lazy(self):
-        r = NicoletIO(filename=None)
-        seg = r.read_segment(lazy=True)
-        for ana in seg.analogsignals:
-            assert isinstance(ana, AnalogSignalProxy)
-            ana = ana.load()
-            assert isinstance(ana, AnalogSignal)
-        for st in seg.spiketrains:
-            assert isinstance(st, SpikeTrainProxy)
-            st = st.load()
-            assert isinstance(st, SpikeTrain)
+        for entity in self.entities_to_test:
+            r = NicoletIO(filename=get_test_file_full_path(self.ioclass, filename=entity, directory=self.local_test_dir))
+            seg = r.read_segment(lazy=True)
+            for ana in seg.analogsignals:
+                assert isinstance(ana, AnalogSignalProxy)
+                ana = ana.load()
+                assert isinstance(ana, AnalogSignal)
 
-        seg = r.read_segment(lazy=False)
-        for anasig in seg.analogsignals:
-            assert isinstance(ana, AnalogSignal)
-            self.assertNotEqual(anasig.size, 0)
-        for st in seg.spiketrains:
-            assert isinstance(st, SpikeTrain)
-            self.assertNotEqual(st.size, 0)
+            seg = r.read_segment(lazy=False)
+            for anasig in seg.analogsignals:
+                assert isinstance(ana, AnalogSignal)
+                self.assertNotEqual(anasig.size, 0)
 
-        # annotations
-        assert "seg_extra_info" in seg.annotations
-        assert seg.name == "Seg #0 Block #0"
-        for anasig in seg.analogsignals:
-            assert anasig.name is not None
-        for st in seg.spiketrains:
-            assert st.name is not None
-        for ev in seg.events:
-            assert ev.name is not None
-        for ep in seg.epochs:
-            assert ep.name is not None
+            # annotations
+            for anasig in seg.analogsignals:
+                assert anasig.name is not None
+            for ev in seg.events:
+                assert ev.name is not None
+            for ep in seg.epochs:
+                assert ep.name is not None
 
     def test_read_block(self):
-        r = NicoletIO(filename=None)
-        bl = r.read_block(lazy=True)
-        # assert len(bl.list_units) == 3
-        # assert len(bl.channel_indexes) == 1 + 1  # signals grouped + units grouped
+        for entity in self.entities_to_test:
+            r = NicoletIO(filename=get_test_file_full_path(self.ioclass, filename=entity, directory=self.local_test_dir))
+            bl = r.read_block(lazy=True)
 
     def test_read_segment_with_time_slice(self):
-        r = NicoletIO(filename=None)
-        seg = r.read_segment(time_slice=None)
-        shape_full = seg.analogsignals[0].shape
-        spikes_full = seg.spiketrains[0]
-        event_full = seg.events[0]
+        for entity in self.entities_to_test:
+            r = NicoletIO(filename=get_test_file_full_path(self.ioclass, filename=entity, directory=self.local_test_dir))
+            seg = r.read_segment(time_slice=None)
+            shape_full = seg.analogsignals[0].shape
+            event_full = seg.events[0]
 
-        t_start, t_stop = 260 * pq.ms, 1.854 * pq.s
-        seg = r.read_segment(time_slice=(t_start, t_stop))
-        shape_slice = seg.analogsignals[0].shape
-        spikes_slice = seg.spiketrains[0]
-        event_slice = seg.events[0]
+            t_start, t_stop = 260 * pq.ms, 1.854 * pq.s
+            seg = r.read_segment(time_slice=(t_start, t_stop))
+            shape_slice = seg.analogsignals[0].shape
+            event_slice = seg.events[0]
 
-        assert shape_full[0] > shape_slice[0]
+            assert shape_full[0] > shape_slice[0]
 
-        assert spikes_full.size > spikes_slice.size
-        assert np.all(spikes_slice >= t_start)
-        assert np.all(spikes_slice <= t_stop)
-        assert spikes_slice.t_start == t_start
-        assert spikes_slice.t_stop == t_stop
-
-        assert event_full.size > event_slice.size
-        assert np.all(event_slice.times >= t_start)
-        assert np.all(event_slice.times <= t_stop)
+            assert event_full.size > event_slice.size
+            assert np.all(event_slice.times >= t_start)
+            assert np.all(event_slice.times <= t_stop)
 
 
 if __name__ == "__main__":
