b61ad00a34232870a8f4de7705a2d2f962099eef
MureziCapaul <murezi.capaul@protonmail.ch>
2025-08-13 11:48:55 +0200

    Add faster readin by reading in entire memap segment

--- neo/rawio/nicoletrawio.py
+++ neo/rawio/nicoletrawio.py
@@ -868,6 +868,7 @@ class NicoletRawIO(BaseRawIO):
             np.cumsum([(segment["duration"].total_seconds()) for segment in self.segments_properties])
         )
         data = np.empty([i_stop - i_start, len(channel_indexes)])
+        section_slices = {}
         for i in range(len(channel_indexes)):
             current_samplingrate = self.segments_properties[seg_index]["sampling_rates"][i]
             [tag_idx] = [tag["index"] for tag in self.tags if tag["tag"] == str(i)]
@@ -884,13 +885,20 @@ class NicoletRawIO(BaseRawIO):
             )
             sections = all_sections[first_section_for_seg:last_section_for_seg]
             sections_length = section_lengths[first_section_for_seg:last_section_for_seg]
-            
+            section_slices[i] = self._get_section_slices(sections, sections_length, i_start, i_stop)
+
+        full_data = np.empty([section_slices[i][-1].stop - section_slices[0][0].start])
+        full_data[0:(section_slices[i][-1].stop - section_slices[0][0].start)] = self.raw_signal[slice(section_slices[0][0].start, section_slices[i][-1].stop)]
+        
+        initial_offset = section_slices[0][0].start
+        for channel, slices in section_slices.items():
             np_idx = 0
-            section_slices = self._get_section_slices(sections, sections_length, i_start, i_stop)
-            for section_slice in section_slices:
-                section_slice_range = section_slice.stop - section_slice.start
-                data[np_idx : (np_idx + section_slice_range), i] = self.raw_signal[section_slice]
-                np_idx += section_slice_range
+            for single_slice in slices:
+                full_data_start = single_slice.start - initial_offset
+                full_data_stop = single_slice.stop - initial_offset
+                slice_length = single_slice.stop - single_slice.start
+                data[np_idx:(np_idx+slice_length),channel] = full_data[slice(full_data_start, full_data_stop)]
+                np_idx += slice_length
 
         return data
 
